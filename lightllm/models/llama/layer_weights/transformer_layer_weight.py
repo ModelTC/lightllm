4448d5d556aeb9ce3536f3429e87a79ebf9c0fa0
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2023-08-14 15:31:54 +0800

    Support Baichuan-7B (#67)

--- lightllm/models/llama/layer_weights/transformer_layer_weight.py
+++ lightllm/models/llama/layer_weights/transformer_layer_weight.py
@@ -24,13 +24,26 @@ class TransformerLayerWeight(BaseLayerWeight):
         self.network_config = network_config
 
     def load_hf_weights(self, weights):
+        # llama and baichuan has same struct, but weight name is not same.
+        if self.network_config.get("model_type", "llama") == "baichuan":
+            # q k v weights for baichuan
+            self.load_baichuan_qkvo_weight(weights)
+        else:
+            # q k v weights for llama
+            self.load_llama_qkvo_weight(weights)
+            
+        # ffn params
+        self.load_ffn_weights(weights)
+        return
+    
+    def load_llama_qkvo_weight(self, weights):
         # input layernorm params
         if f"model.layers.{self.layer_num_}.input_layernorm.weight" in weights:
             self.input_layernorm = weights[f"model.layers.{self.layer_num_}.input_layernorm.weight"].to(self.data_type_).cuda()
 
-        # attention params
         n_embed = self.network_config["hidden_size"]
         split_n_embed = n_embed // self.world_size_
+        # q k v weights for llama
         if f"model.layers.{self.layer_num_}.self_attn.q_proj.weight" in weights:
             self.q_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.q_proj.weight"][split_n_embed *
                                                                                                 self.tp_rank_: split_n_embed * (self.tp_rank_ + 1), :]
@@ -46,19 +59,51 @@ class TransformerLayerWeight(BaseLayerWeight):
                                                                                                 self.tp_rank_: split_n_embed * (self.tp_rank_ + 1), :]
             self.v_weight_ = self.v_weight_.transpose(0, 1).contiguous().to(self.data_type_)
             self.v_weight_ = self.v_weight_.cuda()
-
+        
         # attention output dense params
         if f"model.layers.{self.layer_num_}.self_attn.o_proj.weight" in weights:
             self.att_out_dense_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.o_proj.weight"][:,
                                                                                                             split_n_embed * self.tp_rank_: split_n_embed * (self.tp_rank_ + 1)]
             self.att_out_dense_weight_ = self.att_out_dense_weight_.transpose(0, 1).contiguous().to(self.data_type_)
             self.att_out_dense_weight_ = self.att_out_dense_weight_.cuda()
+        return
+    
+    def load_baichuan_qkvo_weight(self, weights):
+        # input layernorm params
+        if f"model.layers.{self.layer_num_}.input_layernorm.weight" in weights:
+            self.input_layernorm = weights[f"model.layers.{self.layer_num_}.input_layernorm.weight"].to(self.data_type_).cuda()
 
+        n_embed = self.network_config["hidden_size"]
+        split_n_embed = n_embed // self.world_size_
+        # q k v weights for baichuan
+        if f"model.layers.{self.layer_num_}.self_attn.W_pack.weight" in weights:
+            qkv_weights = weights[f"model.layers.{self.layer_num_}.self_attn.W_pack.weight"]
+            split_size = qkv_weights.shape[0] // 3
+            q_weights, k_weights, v_weights = torch.split(qkv_weights, split_size, dim=0)
+
+            self.q_weight_ = q_weights[split_n_embed * self.tp_rank_: split_n_embed * (self.tp_rank_ + 1), :]
+            self.q_weight_ = self.q_weight_.transpose(0, 1).contiguous().to(self.data_type_)
+            self.q_weight_ = self.q_weight_.cuda()
+            self.k_weight_ = k_weights[split_n_embed * self.tp_rank_: split_n_embed * (self.tp_rank_ + 1), :]
+            self.k_weight_ = self.k_weight_.transpose(0, 1).contiguous().to(self.data_type_)
+            self.k_weight_ = self.k_weight_.cuda()
+            self.v_weight_ = v_weights[split_n_embed * self.tp_rank_: split_n_embed * (self.tp_rank_ + 1), :]
+            self.v_weight_ = self.v_weight_.transpose(0, 1).contiguous().to(self.data_type_)
+            self.v_weight_ = self.v_weight_.cuda()
+        
+        # attention output dense params
+        if f"model.layers.{self.layer_num_}.self_attn.o_proj.weight" in weights:
+            self.att_out_dense_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.o_proj.weight"][:,
+                                                                                                            split_n_embed * self.tp_rank_: split_n_embed * (self.tp_rank_ + 1)]
+            self.att_out_dense_weight_ = self.att_out_dense_weight_.transpose(0, 1).contiguous().to(self.data_type_)
+            self.att_out_dense_weight_ = self.att_out_dense_weight_.cuda()
+        return
+    
+    def load_ffn_weights(self, weights):
         if f"model.layers.{self.layer_num_}.post_attention_layernorm.weight" in weights:
             self.post_attention_layernorm_weight_ = weights[f"model.layers.{self.layer_num_}.post_attention_layernorm.weight"].to(
                 self.data_type_).cuda()
-
-        # ffn params
+    
         inter_size = self.network_config['intermediate_size']
         split_inter_size = inter_size // self.world_size_
 
@@ -82,5 +127,4 @@ class TransformerLayerWeight(BaseLayerWeight):
             self.down_proj = self.down_proj.transpose(0, 1).contiguous().to(self.data_type_)
             
             self.down_proj = self.down_proj.cuda()
-
         return
