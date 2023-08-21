a1ef20968d79b45a33e08fba609c64979f95885c
singularity-s0 <12184989+singularity-s0@users.noreply.github.com>
2023-08-21 16:44:05 +0800

    update kv_cache_size calculation

--- lightllm/utils/max_token_num_utils.py
+++ lightllm/utils/max_token_num_utils.py
@@ -1,6 +1,7 @@
 import torch
 torch.multiprocessing.set_start_method('spawn', force=True) # Fork start method will cause CUDA re-initialization error
 import os
+import json
 
 def get_total_free_gpu_memory(tp):
     """
@@ -28,9 +29,31 @@ def get_total_weight_size(weight_dir):
     total_size = total_size / (1024 ** 3)
     return total_size
 
-def calc_max_total_token_num(tp, weight_dir, mem_fill_rate=0.8, kv_cache_size=0.000488281):
+def get_kv_cache_size(model_dir):
+    """
+    Returns the size of the kv cache for a single token, in Gigabytes.
+    """
+    # Read from config.json
+    config_path = os.path.join(model_dir, 'config.json')
+    assert os.path.exists(config_path), "config.json not found in model directory."
+    try:
+        with open(config_path, 'r') as f:
+            config = json.load(f)
+        hidden_size = config['hidden_size']
+        layer_num = config['num_hidden_layers']
+        num_attention_heads = config['num_attention_heads']
+        num_key_value_heads = config.get('num_key_value_heads', num_attention_heads) # Models may not be using GQA
+        dtype = config.get('torch_dtype', 'float16') # TODO: dtype may not be specified in config.json, should we load weights to check?
+    except:
+        raise Exception("Error reading config.json when trying to determine max_total_token_num. Please manually specify max_total_token_num in startup arguments.")
+    dtype_size = torch.empty(0, dtype=getattr(torch, dtype)).element_size()
+    kv_cache_size = hidden_size * dtype_size * 2 * layer_num / num_attention_heads * num_key_value_heads / (1024 ** 3)
+    return kv_cache_size
+
+def calc_max_total_token_num(tp, weight_dir, mem_fill_rate=0.8):
     """
     Calculate the max total token num that can be supported by the model.
     """
+    kv_cache_size = get_kv_cache_size(weight_dir)
     max_token_num = (get_total_free_gpu_memory(tp)-get_total_weight_size(weight_dir)) * mem_fill_rate / kv_cache_size
     return int(max_token_num)
