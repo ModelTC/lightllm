68ac5e4a3956c5a557ea0110609940017579d98b
baishihao <baishihao@sensetime.com>
2023-08-22 23:37:21 +0800

    add support for internlm

---
+++ test/model/test_intern.py
@@ -0,0 +1,21 @@
+import os
+import sys
+import unittest
+from model_infer import test_model_inference
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+class TestInternlmInfer(unittest.TestCase):
+
+    def test_internlm_infer(self):
+        from lightllm.models.internlm.model import InternlmTpPartModel
+        test_model_inference(world_size=8, 
+                             model_dir="/path/internlm-chat-7b/", 
+                             model_class=InternlmTpPartModel, 
+                             batch_size=20, 
+                             input_len=1024, 
+                             output_len=1024)
+        return
+
+
+if __name__ == '__main__':
+    unittest.main()
