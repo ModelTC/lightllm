93009b7e070e990afdd35a3d9d9203bd1bcc58d9
shihaobai <42648726+shihaobai@users.noreply.github.com>
2023-08-24 19:09:50 +0800

    fix the rotary for the chatglm2 (#103)

--- lightllm/models/chatglm2/model.py
+++ lightllm/models/chatglm2/model.py
@@ -53,3 +53,33 @@ class ChatGlm2TpPartModel(Llama2TpPartModel):
         super()._init_some_value()
         self.tp_key_head_num_ = self.config["multi_query_group_num"]
         self.tp_value_head_num_ = self.config["multi_query_group_num"]
+
+    def _init_to_get_rotary(self, base=10000):
+        if self.config.get("rope_scaling", {}) is None:
+            rope_scaling_factor = 1.0
+        else:
+            rope_scaling_factor = self.config.get("rope_scaling", {}).get("factor", 1.0)
+        if "max_sequence_length" in self.config:
+            max_seq_len = self.config["max_sequence_length"]
+        else:
+            max_seq_len = self.config.get("max_position_embeddings", 2048) * rope_scaling_factor
+        base = float(base)
+
+        # NTK
+        try:
+            ntk_alpha = float(os.environ.get("LIGHTLLM_NTK_ALPHA", 1))
+            assert ntk_alpha >= 1
+            if ntk_alpha > 1:
+                print(f"Note: NTK enabled, alpha set to {ntk_alpha}")
+            max_seq_len *= ntk_alpha
+            base = base * (ntk_alpha ** (self.head_dim_ / (self.head_dim_-2))) #Base change formula
+        except:
+            pass
+        n_elem = self.head_dim_ // 2
+        inv_freq = 1.0 / (base ** (torch.arange(0, n_elem, 2, device="cpu", dtype=torch.float32) / n_elem))
+        t = torch.arange(max_seq_len + 1024 * 64, device="cpu", dtype=torch.float32) / rope_scaling_factor
+        freqs = torch.outer(t, inv_freq)
+
+        self._cos_cached = torch.cos(freqs).to(torch.float16).cuda()
+        self._sin_cached = torch.sin(freqs).to(torch.float16).cuda()
+        return
