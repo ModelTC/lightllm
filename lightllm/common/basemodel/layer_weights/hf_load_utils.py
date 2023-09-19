fb20f5ded2a99c8e1d79f303c0b3f2ac5da735db
yaoyongqiang <yaoyongqiang@sensetime.com>
2023-09-19 19:10:37 +0800

    feat: support multiprocess load

--- lightllm/common/basemodel/layer_weights/hf_load_utils.py
+++ lightllm/common/basemodel/layer_weights/hf_load_utils.py
@@ -4,6 +4,22 @@ import gc
 from safetensors import safe_open
 
 
+def load_func(file_, use_safetensors=False, pre_post_layer=None, transformer_layer_list=None, weight_dir=None):
+    if use_safetensors:
+        weights = safe_open(os.path.join(weight_dir, file_), 'pt', 'cpu')
+        weights = {k: weights.get_tensor(k) for k in weights.keys()}
+    else:
+        weights = torch.load(os.path.join(weight_dir, file_), 'cpu')
+
+    if pre_post_layer is not None:
+        pre_post_layer.load_hf_weights(weights)
+    if transformer_layer_list is not None:
+        for layer in transformer_layer_list:
+            layer.load_hf_weights(weights)
+    del weights
+    gc.collect()
+
+
 def load_hf_weights(data_type, weight_dir, pre_post_layer=None, transformer_layer_list=None, weight_dict=None):
     data_type = torch.float16 if data_type == 'fp16' else torch.float32
     if pre_post_layer is not None:
@@ -25,17 +41,10 @@ def load_hf_weights(data_type, weight_dir, pre_post_layer=None, transformer_laye
         use_safetensors = False
         candidate_files = list(filter(lambda x : x.endswith('.bin'), files))
     assert len(candidate_files) != 0, "can only support pytorch tensor and safetensors format for weights."
-    for file_ in candidate_files:
-        if use_safetensors:
-            weights = safe_open(os.path.join(weight_dir, file_), 'pt', 'cpu')
-            weights = {k: weights.get_tensor(k) for k in weights.keys()}
-        else:
-            weights = torch.load(os.path.join(weight_dir, file_), 'cpu')
-        if pre_post_layer is not None:
-            pre_post_layer.load_hf_weights(weights)
-        if transformer_layer_list is not None:
-            for layer in transformer_layer_list:
-                layer.load_hf_weights(weights)
-        del weights
-        gc.collect()
+    from functools import partial
+    from multiprocessing.pool import ThreadPool as Pool
+    partial_func = partial(load_func, use_safetensors=use_safetensors, pre_post_layer=pre_post_layer, transformer_layer_list=transformer_layer_list, weight_dir=weight_dir)  # noqa
+    worker = os.environ.get('LOADWORKER', 8)
+    with Pool(worker) as p:
+        _ = p.map(partial_func, candidate_files)
     return
