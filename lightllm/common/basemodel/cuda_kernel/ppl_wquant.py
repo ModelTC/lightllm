df9110daf4c6258444b233ff6ae22ce7e560c8af
fuheaven <fuheaven@163.com>
2023-11-14 16:22:16 +0800

    fix ppl int4 impl

--- lightllm/common/basemodel/cuda_kernel/ppl_wquant.py
+++ lightllm/common/basemodel/cuda_kernel/ppl_wquant.py
@@ -10,10 +10,10 @@ def quantize_int4_ppl(weight, group_size=128, tp_rank=8):
         q_scale: [K//group_size, N] int32
     """
     weight = weight.transpose(0, 1).contiguous().cuda(tp_rank)
-    from lightllm_ppl_kernel import WeightPreProcess_i4
-    qweight_new, q_scale = WeightPreProcess_i4(weight, group_size=group_size)
-    # from atex.core.ffi import CUDA
-    # qweight_new, q_scale = CUDA.WeightPreProcess_i4(weight, group_size=group_size)
+    # from lightllm_ppl_kernel import WeightPreProcess_i4
+    # qweight_new, q_scale = WeightPreProcess_i4(weight, group_size=group_size)
+    from atex.core.ffi import CUDA
+    qweight_new, q_scale = CUDA.WeightPreProcess_i4(weight, group_size=group_size)
     return qweight_new, q_scale
 
 def matmul_dequantize_int4_ppl(
@@ -30,7 +30,7 @@ def matmul_dequantize_int4_ppl(
     """
     if workspace is None:
         workspace = torch.empty(size=[33554432], dtype=torch.int8, device='cuda') # 32MB workspace
-    from lightllm_ppl_kernel import int4fp16_matmul
-    return int4fp16_matmul(x, qweight, scale_weight, workspace, group_size)
-    # from atex.core.ffi import CUDA
-    # return CUDA.matmul_i4_fp16(x, qweight, scale_weight, workspace, group_size)
+    # from lightllm_ppl_kernel import int4fp16_matmul
+    # return int4fp16_matmul(x, qweight, scale_weight, workspace, group_size)
+    from atex.core.ffi import CUDA
+    return CUDA.matmul_i4_fp16(x, qweight, scale_weight, workspace, group_size)
