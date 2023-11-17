0422e0ccd50c65f231c2cde2278f4a660e7c6d44
wxd000000 <874217876@qq.com>
2023-11-17 15:57:07 +0800

    modify baichuan2_13b

--- lightllm/models/baichuan2_13b/model.py
+++ lightllm/models/baichuan2_13b/model.py
@@ -1,14 +1,17 @@
-from .layer_weights.transformer_layer_weight import BaiChuan2_13bTransformerLayerWeight
-from .layer_infer.transformer_layer_infer import Baichuan2_13bTransformerLayerInfer
+from lightllm.models.baichuan13b.layer_weights.transformer_layer_weight import BaiChuan13bTransformerLayerWeight
+from lightllm.models.baichuan2_7b.layer_weights.pre_and_post_layer_weight import Baichuan2_7bPreAndPostLayerWeight
+from lightllm.models.baichuan13b.layer_infer.transformer_layer_infer import Baichuan13bTransformerLayerInfer
 from lightllm.common.basemodel.infer_struct import InferStateInfo
 from lightllm.models.llama.model import LlamaTpPartModel
 
+
 class Baichuan2_13bTpPartModel(LlamaTpPartModel):
     # weight class
-    transformer_weight_class = BaiChuan2_13bTransformerLayerWeight
+    transformer_weight_class = BaiChuan13bTransformerLayerWeight
+    pre_and_post_weight_class = Baichuan2_7bPreAndPostLayerWeight
 
     # infer class
-    transformer_layer_infer_class = Baichuan2_13bTransformerLayerInfer
+    transformer_layer_infer_class = Baichuan13bTransformerLayerInfer
 
     # infer state class
     infer_state_class = InferStateInfo
