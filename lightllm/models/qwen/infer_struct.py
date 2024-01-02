fix precision of dynamic ntk (#272)
Co-authored-by: baishihao <baishihao@sensetime.com>

lightllm/models/qwen/infer_struct.py
@@ -15,15 +15,24 @@ class QwenInferStateInfo(InferStateInfo):
             b_seq_len_numpy = self.b_seq_len.cpu().numpy()
             position_ids = torch.from_numpy(np.concatenate([np.arange(0, b_seq_len_numpy[i])
                                             for i in range(len(b_seq_len_numpy))], axis=0)).cuda()
-            self.position_cos = torch.index_select(model._cos_cached, 0, position_ids).view(position_ids.shape[0], -1)
-            self.position_sin = torch.index_select(model._sin_cached, 0, position_ids).view(position_ids.shape[0], -1)
+            infer_ntk_alphas = np.maximum(2 ** (np.ceil(np.log2(b_seq_len_numpy / model.config.get("seq_length", 2048)) + 1)), 1)
+            model.infer_ntk_id = torch.tensor([model._ntk_to_id[i] for i in infer_ntk_alphas]).cuda()
+            self.position_sin = []
+            self.position_cos = []
+            model.infer_ntk_id = [model._ntk_to_id[i] for i in infer_ntk_alphas]
+            for i in range(len(infer_ntk_alphas)):
+                self.position_sin.append(model._sin_cached[model.infer_ntk_id[i]][position_ids[self.b_start_loc[i]: self.b_start_loc[i] + self.b_seq_len[i]]])
+                self.position_cos.append(model._cos_cached[model.infer_ntk_id[i]][position_ids[self.b_start_loc[i]: self.b_start_loc[i] + self.b_seq_len[i]]])
+
+            self.position_sin = torch.cat(self.position_sin, dim=0)
+            self.position_cos = torch.cat(self.position_cos, dim=0)
             if model.logn_tensor is not None:
                 self.logn_values = torch.index_select(model.logn_tensor, 0, position_ids).view(-1)
             position_ids = None
         else:
             position_ids = self.b_seq_len - 1
-            self.position_cos = torch.index_select(model._cos_cached, 0, position_ids).view(self.b_seq_len.shape[0], -1)
-            self.position_sin = torch.index_select(model._sin_cached, 0, position_ids).view(self.b_seq_len.shape[0], -1)
+            self.position_cos = model._cos_cached[model.infer_ntk_id, position_ids].view(position_ids.shape[0], -1)
+            self.position_sin = model._sin_cached[model.infer_ntk_id, position_ids].view(position_ids.shape[0], -1)
             if model.logn_tensor is not None:
                 self.logn_values = torch.index_select(model.logn_tensor, 0, position_ids).view(-1)
             self.other_kv_index = self.req_manager.req_to_token_indexs[self.b_req_idx[0], 0].item()

lightllm/models/qwen/model.py
@@ -2,6 +2,7 @@ import os
 import json
 import torch
 import math
+import numpy as np
 
 from .layer_infer.transformer_layer_infer import QwenTransformerLayerInfer
 from .layer_weights.pre_and_post_layer_weight import QwenPreAndPostLayerWeight
@@ -45,31 +46,44 @@ class QWenTpPartModel(LlamaTpPartModel):
             super()._init_custom()
             self.logn_tensor = None
         return
-    
+
+    def _init_nkt_alpha(self, total_seq_len_supported):
+        self._ntk_to_id = {}
+        ntk_alphas = []
+        for seq_len in range(1, total_seq_len_supported + 1):
+            ntk_alpha = max(2 ** math.ceil(math.log(seq_len / 2048, 2) + 1), 1)
+            self._ntk_to_id[ntk_alpha] = len(self._ntk_to_id) - 1
+            ntk_alphas.append(ntk_alpha)
+        ntk_alphas = np.array(ntk_alphas, dtype=np.int32)
+        return np.unique(ntk_alphas)
+
     def _init_qwen_dynamic_ntk(self):
         total_seq_len_supported = self.config.get("max_position_embeddings", 8 * 1024)
         seq_len = self.config.get("seq_length", 2048)
 
-        cur_kv_seq_len = total_seq_len_supported
-        context_value = math.log(cur_kv_seq_len / seq_len, 2) + 1
-        ntk_alpha = 2 ** math.ceil(context_value) - 1
-        ntk_alpha = max(ntk_alpha, 1)
+        ntk_alphas = self._init_nkt_alpha(total_seq_len_supported)
+        self._cos_cached = []
+        self._sin_cached = []
 
-        base = self.config.get("rotary_emb_base", 10000)
-        base = base * ntk_alpha ** (self.head_dim_ / (self.head_dim_ - 2))
-        inv_freq = 1.0 / (
-            base
-            ** (
-                torch.arange(0, self.head_dim_, 2, device="cpu", dtype=torch.float32)
-                / self.head_dim_
+        for ntk_alpha in ntk_alphas:
+
+            base = self.config.get("rotary_emb_base", 10000)
+            base = base * ntk_alpha ** (self.head_dim_ / (self.head_dim_ - 2))
+            inv_freq = 1.0 / (
+                base
+                ** (
+                    torch.arange(0, self.head_dim_, 2, device="cpu", dtype=torch.float32)
+                    / self.head_dim_
+                )
             )
-        )
 
-        t = torch.arange(total_seq_len_supported + 64 * 1024, device="cpu", dtype=torch.float32)
-        freqs = torch.outer(t, inv_freq)
+            t = torch.arange(total_seq_len_supported + 64 * 1024, device="cpu", dtype=torch.float32)
+            freqs = torch.outer(t, inv_freq)
+            self._cos_cached.append(torch.cos(freqs).to(torch.float16).cuda())
+            self._sin_cached.append(torch.sin(freqs).to(torch.float16).cuda())
 
-        self._cos_cached = torch.cos(freqs).to(torch.float16).cuda()
-        self._sin_cached = torch.sin(freqs).to(torch.float16).cuda()
+        self._cos_cached = torch.stack(self._cos_cached, dim=0).contiguous()
+        self._sin_cached = torch.stack(self._sin_cached, dim=0).contiguous()
         return
     
     def _init_qwen_logn_attn(self):