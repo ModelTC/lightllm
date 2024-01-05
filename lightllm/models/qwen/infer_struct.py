1fc3bd706d7fa8dead7ef8e45823ff27d0c439fa
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2024-01-05 15:54:21 +0800

    fix for torch 1.13

--- lightllm/models/qwen/infer_struct.py
+++ lightllm/models/qwen/infer_struct.py
@@ -12,15 +12,16 @@ class QwenInferStateInfo(InferStateInfo):
 
     def init_some_extra_state(self, model, input_ids : torch.Tensor):
         if self.is_prefill:
+            b_start_loc_numpy = self.b_start_loc.cpu().numpy() 
             b_seq_len_numpy = self.b_seq_len.cpu().numpy()
             position_ids = torch.from_numpy(np.concatenate([np.arange(0, b_seq_len_numpy[i])
                                             for i in range(len(b_seq_len_numpy))], axis=0)).cuda()
             self.position_sin = []
             self.position_cos = []
-            infer_ntk_id = torch.clamp(torch.ceil(torch.log2(self.b_seq_len / model.config.get("seq_length", 2048)) + 1), 0, model.max_ntk_alpha).int()
+            infer_ntk_id = torch.clamp(torch.ceil(torch.log2(self.b_seq_len / model.config.get("seq_length", 2048)) + 1), 0, model.max_ntk_alpha).long()
             for i in range(len(infer_ntk_id)):
-                self.position_sin.append(model._sin_cached[infer_ntk_id[i]][position_ids[self.b_start_loc[i]: self.b_start_loc[i] + self.b_seq_len[i]]])
-                self.position_cos.append(model._cos_cached[infer_ntk_id[i]][position_ids[self.b_start_loc[i]: self.b_start_loc[i] + self.b_seq_len[i]]])
+                self.position_sin.append(model._sin_cached[infer_ntk_id[i]][position_ids[b_start_loc_numpy[i]: b_start_loc_numpy[i] + b_seq_len_numpy[i]]])
+                self.position_cos.append(model._cos_cached[infer_ntk_id[i]][position_ids[b_start_loc_numpy[i]: b_start_loc_numpy[i] + b_seq_len_numpy[i]]])
 
             self.position_sin = torch.cat(self.position_sin, dim=0)
             self.position_cos = torch.cat(self.position_cos, dim=0)
@@ -28,8 +29,8 @@ class QwenInferStateInfo(InferStateInfo):
                 self.logn_values = torch.index_select(model.logn_tensor, 0, position_ids).view(-1)
             position_ids = None
         else:
-            infer_ntk_id = torch.clamp(torch.ceil(torch.log2(self.b_seq_len / model.config.get("seq_length", 2048)) + 1), 0, model.max_ntk_alpha).int()
-            position_ids = self.b_seq_len - 1
+            infer_ntk_id = torch.clamp(torch.ceil(torch.log2(self.b_seq_len / model.config.get("seq_length", 2048)) + 1), 0, model.max_ntk_alpha).long()
+            position_ids = (self.b_seq_len - 1).long()
             self.position_cos = model._cos_cached[infer_ntk_id, position_ids].view(position_ids.shape[0], -1)
             self.position_sin = model._sin_cached[infer_ntk_id, position_ids].view(position_ids.shape[0], -1)
             if model.logn_tensor is not None:
