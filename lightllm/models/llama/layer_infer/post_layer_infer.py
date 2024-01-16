234aeb5fc305242c6e7d619c688e0ad5d812cd35
wangzaijun <wangzaijun@sensetime.com>
2024-01-16 17:20:04 +0800

    fix bug when vob_size % world_size != 0

--- lightllm/models/llama/layer_infer/post_layer_infer.py
+++ lightllm/models/llama/layer_infer/post_layer_infer.py
@@ -12,9 +12,9 @@ from lightllm.models.llama.triton_kernel.rmsnorm import rmsnorm_forward
 from lightllm.common.basemodel import PostLayerInferTpl
 from lightllm.utils.infer_utils import mark_cost_time
 
+
 class LlamaPostLayerInfer(PostLayerInferTpl):
-    """
-    """
+    """ """
 
     def __init__(self, tp_rank, world_size, network_config, mode):
         super().__init__(tp_rank, world_size, network_config, mode)
@@ -22,20 +22,26 @@ class LlamaPostLayerInfer(PostLayerInferTpl):
         self.vocab_size_ = network_config["vocab_size"]
         self.embed_dim_ = network_config["n_embed"]
         return
-    
-    def _norm(self, input, infer_state, layer_weight:LlamaPreAndPostLayerWeight) -> torch.Tensor:
+
+    def _norm(self, input, infer_state, layer_weight: LlamaPreAndPostLayerWeight) -> torch.Tensor:
         return rmsnorm_forward(input, layer_weight.final_norm_weight_, eps=self.eps_)
-    
+
     def _slice_get_last_input(self, input_embdings, infer_state: LlamaInferStateInfo):
         if infer_state.is_splitfuse:
             # for SplitFuse
             batch_size = infer_state.batch_size
             last_input = torch.empty((batch_size, self.embed_dim_), device=input_embdings.device, dtype=torch.float16)
-            tmp_ = torch.cat([torch.ones(infer_state.decode_req_num, dtype=torch.int32, device="cuda"), infer_state.prefill_b_split_seq_len], dim=0)
+            tmp_ = torch.cat(
+                [
+                    torch.ones(infer_state.decode_req_num, dtype=torch.int32, device="cuda"),
+                    infer_state.prefill_b_split_seq_len,
+                ],
+                dim=0,
+            )
             last_index = torch.cumsum(tmp_, dim=0, dtype=torch.long) - 1
             last_input[:, :] = input_embdings[last_index, :]
             return last_input, batch_size
-        
+
         if not infer_state.is_splitfuse and infer_state.is_prefill and not infer_state.return_all_prompt_logprobs:
             batch_size = infer_state.batch_size
             last_input = torch.empty((batch_size, self.embed_dim_), device=input_embdings.device, dtype=torch.float16)
@@ -46,18 +52,23 @@ class LlamaPostLayerInfer(PostLayerInferTpl):
         if not infer_state.is_splitfuse and infer_state.is_prefill and infer_state.return_all_prompt_logprobs:
             total_tokens = infer_state.total_token_num
             return input_embdings, total_tokens
-        
+
         if not infer_state.is_splitfuse and not infer_state.is_prefill:
             batch_size = infer_state.batch_size
             return input_embdings[-batch_size:, :], batch_size
-        
-        assert False, "Error State"
 
+        assert False, "Error State"
 
     def soft_max(self, data):
         return torch.softmax(data.permute(1, 0).float(), dim=-1)
 
-    def token_forward(self, input_embdings, infer_state: LlamaInferStateInfo, layer_weight: LlamaPreAndPostLayerWeight, return_logics=False):
+    def token_forward(
+        self,
+        input_embdings,
+        infer_state: LlamaInferStateInfo,
+        layer_weight: LlamaPreAndPostLayerWeight,
+        return_logics=False,
+    ):
         last_input, token_num = self._slice_get_last_input(input_embdings, infer_state)
         input_embdings = None
         last_input = self._norm(last_input, infer_state, layer_weight)
@@ -69,9 +80,13 @@ class LlamaPostLayerInfer(PostLayerInferTpl):
             gather_data = logic_batch
         else:
             gather_data = torch.empty((self.vocab_size_, token_num), device=logic_batch.device, dtype=torch.float16)
-            split_size = self.vocab_size_ // self.world_size_
-            dist.all_gather([gather_data[i * split_size: (i + 1) * split_size, :]
-                            for i in range(self.world_size_)], logic_batch, group=None, async_op=False)
+            split_indexes = np.linspace(0, self.vocab_size_, self.world_size_ + 1, dtype=np.int64)
+            dist.all_gather(
+                [gather_data[split_indexes[i] : split_indexes[i + 1], :] for i in range(self.world_size_)],
+                logic_batch,
+                group=None,
+                async_op=False,
+            )
         logic_batch = None
 
         if not return_logics:
@@ -82,7 +97,9 @@ class LlamaPostLayerInfer(PostLayerInferTpl):
             ans_logics = gather_data.permute(1, 0).float()
             gather_data = None
             return ans_logics
-    
+
     # @mark_cost_time("splitfuse post forward")
-    def splitfuse_forward(self, input_embdings, infer_state: SplitFuseInferStateInfo, layer_weight: BaseLayerWeight, return_logics=False):
+    def splitfuse_forward(
+        self, input_embdings, infer_state: SplitFuseInferStateInfo, layer_weight: BaseLayerWeight, return_logics=False
+    ):
         return self.token_forward(input_embdings, infer_state, layer_weight, return_logics=return_logics)
--- lightllm/models/llama/layer_weights/pre_and_post_layer_weight.py
+++ lightllm/models/llama/layer_weights/pre_and_post_layer_weight.py
@@ -10,26 +10,23 @@ class LlamaPreAndPostLayerWeight(PreAndPostLayerWeight):
 
     def load_hf_weights(self, weights):
         vob_size = self.network_config_["vocab_size"]
-        split_vob_size = vob_size // self.world_size_
-        n_embed = self.network_config_["hidden_size"]
+        split_indexes = np.linspace(0, vob_size, self.world_size_ + 1, dtype=np.int64)
+        split_start = split_indexes[self.tp_rank_]
+        split_end = split_indexes[self.tp_rank_ + 1]
         if "model.embed_tokens.weight" in weights:
             # print(weights['model.embed_tokens.weight'].shape)
-            self.wte_weight_ = self._cuda(weights['model.embed_tokens.weight'][split_vob_size *
-                                                                    self.tp_rank_: split_vob_size * (self.tp_rank_ + 1), :])
-        if 'lm_head.weight' in weights:
+            self.wte_weight_ = self._cuda(weights["model.embed_tokens.weight"][split_start:split_end, :])
+        if "lm_head.weight" in weights:
             # print(weights['lm_head.weight'].shape)
-            self.lm_head_weight_ = self._cuda(weights['lm_head.weight'][split_vob_size * self.tp_rank_: split_vob_size *
-                                                            (self.tp_rank_ + 1), :])
-        if 'model.norm.weight' in weights:
-            self.final_norm_weight_ = self._cuda(weights['model.norm.weight'])
+            self.lm_head_weight_ = self._cuda(weights["lm_head.weight"][split_start:split_end, :])
+        if "model.norm.weight" in weights:
+            self.final_norm_weight_ = self._cuda(weights["model.norm.weight"])
 
         return
-    
+
     def verify_load(self):
         errors = "weights load not ok"
-        weights = [self.wte_weight_, 
-                   self.lm_head_weight_, 
-                   self.final_norm_weight_]
+        weights = [self.wte_weight_, self.lm_head_weight_, self.final_norm_weight_]
         for i in range(len(weights)):
             assert weights[i] is not None, "index:" + str(i) + " " + errors
-        return 
+        return
