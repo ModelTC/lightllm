1f1c37e7d700ed4cc9474faff488884a63f86763
baishihao <baishihao@sensetime.com>
2024-02-20 13:50:59 +0800

    read the img size from the config

--- lightllm/models/llava/model.py
+++ lightllm/models/llava/model.py
@@ -1,3 +1,4 @@
+import re
 import json
 import numpy as np
 from lightllm.models.llama.model import LlamaTpPartModel
@@ -8,11 +9,20 @@ from lightllm.server.multimodal_params import MultimodalParams
 # Warp of the origal tokenizer
 class LlavaTokenizer:
 
-    def __init__(self, tokenizer):
+    def __init__(self, tokenizer, model_cfg):
         self.tokenizer = tokenizer
         self.image_token = "<image>"
-        # (image_size // patch_size) ** 2: (336 // 14) ** 2
-        self.image_length = 576
+        mm_vision_tower = model_cfg.get('mm_vision_tower', 'openai/clip-vit-large-patch14-336')
+        if isinstance(mm_vision_tower, list):
+            mm_vision_tower = mm_vision_tower[0]
+        mm_vision_tower = mm_vision_tower.split('/')[-1]
+        vision_tower_match = re.match(r'^clip-vit-large-patch(\d+)-(\d+)$', mm_vision_tower)
+        patch_size = int(vision_tower_match.group(1))
+        default_img_size = int(vision_tower_match.group(2))
+        image_size = model_cfg.get("img_size", default_img_size)
+        image_size = model_cfg.get("mm_image_size", image_size)
+        # (image_size // patch_size) ** 2: (336 // 14) ** 2 = 576    
+        self.image_length = (image_size // patch_size) ** 2
 
     # only change the impl of the encode func:
     def encode(self, prompt, multimodal_params: MultimodalParams = None):
