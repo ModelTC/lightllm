47eecaaefd202f6dc6a640a11f15972f3f7d9206
huakaigo <liuahuakai@sensetime.com>
2024-03-14 18:32:50 +0800

    fix weights loadng when tp > 1 in llama_quik

--- lightllm/models/llama_quik/layer_weights/transformer_layer_weight.py
+++ lightllm/models/llama_quik/layer_weights/transformer_layer_weight.py
@@ -1,14 +1,7 @@
-import math
-from fsspec import Callback
-
-import numpy as np
 import torch
-from functools import partial
 from typing import Callable, Dict
 
 from lightllm.common.basemodel import TransformerLayerWeight
-from lightllm.common.basemodel.cuda_kernel.ppl_awquant import dynamic_channelwise_quant_fp16_i8_ppl
-
 from .qlinear import MixedQLinear, SharedQuantizedInput
 
 class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
@@ -77,7 +70,7 @@ class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
                 unexpected = current - expect
                 assert len(missings) == 0 and len(unexpected) == 0, f"index: {i} weights load not ok, missings = {missings}, unexpected = {unexpected}"
 
-        # TODO 为了避免重写load,在这里组织projs
+        # NOTE load projs here to avoid override model._init_weights()
         self._load_projs()
         self._verify_proj()
 
@@ -119,14 +112,13 @@ class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
                 kv_weights[key] = first_weights[key]
             return kv_weights
 
-    def _load_column_splitting_proj(self, prefix: str, split_n: int, weights: Dict[str, torch.Tensor]):
-        """load q/k/v_proj in attention module and gate/up_proj in mlp module, , which are column splitting when tensor parallel is enabled
+    def _load_weights(self, prefix: str, weights: Dict[str, torch.Tensor]):
+        """load the weights of projection
 
         Args:
             prefix (str): The weight prefix in the weights dictionary. 
                 Example: "model.layers.0.self_attn.q_proj", "model.layers.0.mlp.gate_proj".
-            split_n (int): The number of splits.
-            weights (Dict[str, torch.Tensor]): The weights dictionary.
+            weights (Dict[str, torch.Tensor]): The weights dictionary, reduced_w's shape is [1, N] and other weights' shape are [N, K].
 
         Returns:
             Dict[str, torch.Tensor]
@@ -135,107 +127,130 @@ class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
         collector = {}
 
         # load weights
-        for name in self.WEIGHT_KEYS:
+        for name in self.ALL_WEIGHT_KEYS:
             full_name = _construt_weight_key_fn(name)
             if full_name not in weights:
                 continue
-            weight_ = weights[full_name]
-            weight_ = weight_[split_n * self.tp_rank_ : split_n * (self.tp_rank_ + 1), :]
-            collector[name] = weight_
-
-        # load indices
-        for name in self.INDICE_KEYS:
-            if full_name not in weights:
-                continue
-            full_name = _construt_weight_key_fn(name)
-            weight_ = weights[full_name]
-            # weight_ = weight_.to(torch.int32)
-            collector[name] = weight_
+            collector[name] = weights[full_name]
 
         return collector
 
-    def _load_row_splitting_proj(self, prefix: str, split_n: int, weights: Dict[str, torch.Tensor]):
-        """load o_proj in attention module and down_proj in mlp module, which are row splitting when tensor parallel is enabled
+
+    def _split_weights_by_column(self, weights: Dict[str, torch.Tensor], split_n: int):
+        """split q/k/v_proj in attention module and gate/up_proj in mlp module, which are column splitting when tensor parallel is enabled
 
         Args:
-            prefix (str): The weight prefix in the weights dictionary. 
-                Example: "model.layers.0.self_attn.o_proj", "model.layers.0.mlp.down_proj".
+                Example: "model.layers.0.self_attn.q_proj", "model.layers.0.mlp.gate_proj".
             split_n (int): The number of splits.
-            weights (Dict[str, torch.Tensor]): The weights dictionary.
+            weights (Dict[str, torch.Tensor]): The weights dictionary, reduced_w's shape is [1, N] and other weights' shape are [N, K].
 
         Returns:
             Dict[str, torch.Tensor]
         """
-        _construt_weight_key_fn = lambda x: f"{prefix}.{x}"
+        if self.world_size_ == 1:
+            return weights
+
         collector = {}
 
         # load weights
-        lower = split_n * self.tp_rank_
-        upper = split_n * (self.tp_rank_ + 1)
         for name in self.WEIGHT_KEYS:
-            full_name = _construt_weight_key_fn(name)
-            if full_name not in weights:
-                continue
-            weight_ = weights[full_name]
-            if name in ["fp_weight", "int_weight"]:
-                weight_ = weight_[:, lower : upper]
-            # @TODO: 当TP>1时，有几个个问题：
-            ## 1. reduced_w处理是近似变换, 不严格相等
-            ## 2. 如果int_indices分配到不同卡上的数量出现奇数，当前activation量化算法会失败(要求 num_int % 2 != 0)
-            ## 3. fp_indices和int_indices不是均匀分布，跑TP时会出现推理耗时不均匀的问题
-            ## 要解决此问题有两种方法
-            ## 1. 将weight按TP拆分后量化
-            ## 2. 行拆分的proj不做TP => 减弱TP性能加速效果
-            elif name in ["reduced_w"]:
-                weight_ = weight_ / self.world_size_
+            weight_ = weights[name]
+            if name == "reduced_w":
+                weight_ = weight_[:, split_n * self.tp_rank_ : split_n * (self.tp_rank_ + 1)]
+            else:
+                weight_ = weight_[split_n * self.tp_rank_ : split_n * (self.tp_rank_ + 1), :]
             collector[name] = weight_
 
         # load indices
         for name in self.INDICE_KEYS:
-            full_name = _construt_weight_key_fn(name)
-            if full_name not in weights:
-                continue
-            weight_ = weights[full_name]
-            # only indices in range [lower, upper) are reserved
-            selected = torch.logical_and(weight_ >= lower, weight_ < upper)
-            weight_ = weight_[selected] - lower
+            weight_ = weights[name]
             # weight_ = weight_.to(torch.int32)
-            # weight_ = self._cuda(weight_)
             collector[name] = weight_
 
         return collector
 
-    def _load_proj(self, name_hint:str, weights: Dict[str, torch.Tensor], shared_input: SharedQuantizedInput = None) -> MixedQLinear:
-        if not weights:
-            return None
-        try:
-            proj = MixedQLinear.from_dict(name_hint, weights, shared_input)
-            if self.tp_rank_ is None:
-                return proj.eval().cuda()
-            else:
-                return proj.eval().cuda(self.tp_rank_)
-        except Exception as e:
-            print(str(e))
-            return None
+    def _split_weights_by_row(self, weights: Dict[str, torch.Tensor], split_n: int):
+        """split o_proj in attention module and down_proj in mlp module, which are row splitting when tensor parallel is enabled
+
+        Args:
+            split_n (int): The number of splits.
+            weights (Dict[str, torch.Tensor]): The weights dictionary, reduced_w's shape is [1, N] and other weights' shape are [N, K].
+
+        Returns:
+            Dict[str, torch.Tensor]
+        """
+        if self.world_size_ == 1:
+            return weights
+
+        collector = {}
+
+        lower = split_n * self.tp_rank_
+        upper = split_n * (self.tp_rank_ + 1)
+
+        fp_indices_num = weights["fp_indices"].shape[0]
+        int_indices_num = weights["int_indices"].shape[0]
+
+        N = weights["int_weight"].shape[0]
+        K = fp_indices_num + int_indices_num
+
+        bits = weights["int_weight"].shape[1] * 8 // int_indices_num
+        assert bits in [4, 8], f"quant bits should be one of [4, 8], but get {bits}"
+
+        # load indices
+        weight_ = weights["fp_indices"]
+        # only indices in range [lower, upper) are reserved
+        fp_selected = torch.logical_and(weight_ >= lower, weight_ < upper)
+        collector["fp_indices"] = weight_[fp_selected]
+
+        weight_ = weights["int_indices"]
+        # only indices in range [lower, upper) are reserved
+        int_selected = torch.logical_and(weight_ >= lower, weight_ < upper)
+        collector["int_indices"] = weight_[int_selected]
+
+        # load weights
+        # import pdb; pdb.set_trace()
+        weight_ = weights["fp_weight"]
+        collector["fp_weight"] = weight_[:, fp_selected]
+
+        weight_ = weights["int_weight"]
+        if bits == 8:
+            collector["int_weight"] = weight_[:, int_selected]
+            dequant_weight_ = collector["int_weight"] * weights["weights_scales"]
+            collector["reduced_w"] = torch.sum(dequant_weight_, dim = 1, dtype = torch.float16).unsqueeze(0)
+        else:
+            in_dim, out_dim = weight_.shape
+            out_dim = out_dim * 2 # uint8 unpack to [int4, int4]
+            assert int_selected.shape[0] % 2 == 0, f"TP is {self.world_size_}, total indices is {out_dim}. rank[{self.tp_rank_}] indices is {int_selected.shape[0]}"
+
+            unpack = weight_.flatten().unsqueeze(1)
+            unpack = unpack.repeat([1, 2])
+            unpack[:, 0] = unpack[:, 0] & 0xf
+            unpack[:, 1] = (unpack[:, 1] >> 4) & 0xf
+            unpack = unpack.view(in_dim, out_dim)
+            weight_ = unpack[:, int_selected]
+            # reinterpret int4 weight by bits
+            int4_weight_ = (weight_.to(torch.int8) << 4) >> 4
+            dequant_weight_ = int4_weight_ * weights["weights_scales"]
+            collector["reduced_w"] = torch.sum(dequant_weight_, dim = 1, dtype = torch.float16).unsqueeze(0)
+            # repack two int4 to uint8
+            collector["int_weight"] = weight_[:, 0::2] | (weight_[:, 1::2] << 4)
+
+        collector["weights_scales"] = weights["weights_scales"]
+
+        for name in self.INDICE_KEYS:
+            collector[name] = collector[name] - lower
+
+        return collector
 
     def _load_qkvo_weights(self, weights):
         # input layernorm params
         if f"model.layers.{self.layer_num_}.input_layernorm.weight" in weights:
             self.att_norm_weight_ = self._cuda(weights[f"model.layers.{self.layer_num_}.input_layernorm.weight"])
 
-        n_embed = self.network_config_["hidden_size"]
-        q_split_n_embed = n_embed // self.world_size_
-        kv_split_n_embed = (
-            n_embed
-            // self.network_config_["num_attention_heads"]
-            * self.network_config_["num_key_value_heads"]
-            // self.world_size_
-        )
-
-        q_weights_ = self._load_column_splitting_proj(f"model.layers.{self.layer_num_}.self_attn.q_proj", q_split_n_embed, weights)
-        k_weights_ = self._load_column_splitting_proj(f"model.layers.{self.layer_num_}.self_attn.k_proj", kv_split_n_embed, weights)
-        v_weights_ = self._load_column_splitting_proj(f"model.layers.{self.layer_num_}.self_attn.v_proj", kv_split_n_embed, weights)
-        o_weights_ = self._load_row_splitting_proj(f"model.layers.{self.layer_num_}.self_attn.o_proj", q_split_n_embed, weights)
+        q_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.self_attn.q_proj", weights)
+        k_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.self_attn.k_proj", weights)
+        v_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.self_attn.v_proj", weights)
+        o_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.self_attn.o_proj", weights)
         with self.lock:
             self._q_weights.update(q_weights_)
             self._k_weights.update(k_weights_)
@@ -250,12 +265,9 @@ class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
                 weights[f"model.layers.{self.layer_num_}.post_attention_layernorm.weight"]
             )
 
-        inter_size = self.network_config_["intermediate_size"]
-        split_inter_size = inter_size // self.world_size_
-
-        gate_weights_ = self._load_column_splitting_proj(f"model.layers.{self.layer_num_}.mlp.gate_proj", split_inter_size, weights)
-        up_weights_ = self._load_column_splitting_proj(f"model.layers.{self.layer_num_}.mlp.up_proj", split_inter_size, weights)
-        down_weights_ = self._load_row_splitting_proj(f"model.layers.{self.layer_num_}.mlp.down_proj", split_inter_size, weights)
+        gate_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.mlp.gate_proj", weights)
+        up_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.mlp.up_proj", weights)
+        down_weights_ = self._load_weights(f"model.layers.{self.layer_num_}.mlp.down_proj", weights)
 
         with self.lock:
             self._gate_weights.update(gate_weights_)
@@ -264,19 +276,46 @@ class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
 
         return
 
+    def _load_proj(self, name_hint:str, weights: Dict[str, torch.Tensor], shared_input: SharedQuantizedInput = None) -> MixedQLinear:
+        if not weights:
+            return None
+        try:
+            proj = MixedQLinear.from_dict(name_hint, weights, shared_input)
+            if self.tp_rank_ is None:
+                return proj.eval().cuda()
+            else:
+                return proj.eval().cuda(self.tp_rank_)
+        except Exception as e:
+            print(str(e))
+            return None
+
     def _load_qkvo_proj(self):
+        n_embed = self.network_config_["hidden_size"]
+        q_split_n_embed = n_embed // self.world_size_
+        kv_split_n_embed = (
+            n_embed
+            // self.network_config_["num_attention_heads"]
+            * self.network_config_["num_key_value_heads"]
+            // self.world_size_
+        )
+
+        self._q_weights = self._split_weights_by_column(self._q_weights, q_split_n_embed)
+        self._k_weights = self._split_weights_by_column(self._k_weights, kv_split_n_embed)
+        self._v_weights = self._split_weights_by_column(self._v_weights, kv_split_n_embed)
+        self._o_weights = self._split_weights_by_row(self._o_weights, q_split_n_embed)
+
         shared_input_n = 2 if self.cat_kv_ else 3
         qkv_shared_input = SharedQuantizedInput(shared_input_n)
         self.q_proj = self._load_proj(f"model.layers.{self.layer_num_}.self_attn.q_proj", self._q_weights, qkv_shared_input)
         self._q_weights = None
-        # split k and v
+        # cat k and v
         if self.cat_kv_:
             kv_weights = self._try_cat_tensors(self._k_weights, self._v_weights)
             self.k_weights_ = None
             self.v_weights_ = None
             self.kv_proj: MixedQLinear = self._load_proj(f"model.layers.{self.layer_num_}.self_attn.kv_proj", kv_weights, qkv_shared_input)
             kv_weights = None
-        else: # cat k and v
+        else: # split k and v
             self.k_proj: MixedQLinear = self._load_proj(f"model.layers.{self.layer_num_}.self_attn.k_proj", self._k_weights, qkv_shared_input)
             self.v_proj: MixedQLinear = self._load_proj(f"model.layers.{self.layer_num_}.self_attn.v_proj", self._v_weights, qkv_shared_input)
             self._k_weights = None
@@ -288,6 +327,13 @@ class LlamaTransformerLayerWeightQuik(TransformerLayerWeight):
         return
 
     def _load_ffn_proj(self):
+        inter_size = self.network_config_["intermediate_size"]
+        split_inter_size = inter_size // self.world_size_
+
+        self._gate_weights = self._split_weights_by_column(self._gate_weights, split_inter_size)
+        self._up_weights = self._split_weights_by_column(self._up_weights, split_inter_size)
+        self._down_weights = self._split_weights_by_row(self._down_weights, split_inter_size)
+
         shared_input_n = 1 if self.cat_gate_up_ else 2
         gate_up_shared_input = SharedQuantizedInput(shared_input_n)
         if self.cat_gate_up_:
