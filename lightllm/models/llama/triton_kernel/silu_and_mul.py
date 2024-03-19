bb191c4ad9b88569fa998971883a00bfab617e9d
Chen Junyi <chenjunyi@sensetime.com>
2024-03-19 17:20:10 +0800

    fix

--- lightllm/models/llama/triton_kernel/silu_and_mul.py
+++ lightllm/models/llama/triton_kernel/silu_and_mul.py
@@ -3,57 +3,56 @@ import torch
 import triton
 import triton.language as tl
 
+if triton.__version__ >= "2.1.0":
 
-@triton.jit
-def _silu_and_mul_kernel(
-    input_ptr,
-    stride_input_m,
-    stride_input_n,
-    stride_output_m,
-    stride_output_n,
-    size_m,
-    size_n,
-    BLOCK_M: tl.constexpr,
-    BLOCK_N: tl.constexpr,
-):
-    stride_input_m = stride_input_m.to(tl.int64)
-    stride_output_m = stride_output_m.to(tl.int64)
+    @triton.jit
+    def _silu_and_mul_kernel(
+        input_ptr,
+        stride_input_m,
+        stride_input_n,
+        stride_output_m,
+        stride_output_n,
+        size_m,
+        size_n,
+        BLOCK_M: tl.constexpr,
+        BLOCK_N: tl.constexpr,
+    ):
+        stride_input_m = stride_input_m.to(tl.int64)
+        stride_output_m = stride_output_m.to(tl.int64)
 
-    tid = tl.program_id(0)
-    input_m_offsets = tid * BLOCK_M + tl.arange(0, BLOCK_M)
-    output_m_offsets = tid * BLOCK_M + tl.arange(0, BLOCK_M)
+        tid = tl.program_id(0)
+        input_m_offsets = tid * BLOCK_M + tl.arange(0, BLOCK_M)
+        output_m_offsets = tid * BLOCK_M + tl.arange(0, BLOCK_M)
 
-    pid = tl.program_id(1)
-    input_n_offsets = pid * BLOCK_N + tl.arange(0, BLOCK_N)
-    output_n_offsets = pid * BLOCK_N + tl.arange(0, BLOCK_N)
+        pid = tl.program_id(1)
+        input_n_offsets = pid * BLOCK_N + tl.arange(0, BLOCK_N)
+        output_n_offsets = pid * BLOCK_N + tl.arange(0, BLOCK_N)
 
-    up_offsets = input_m_offsets[:, None] * stride_input_m + (input_n_offsets[None, :] + size_n) * stride_input_n
-    gate_offsets = input_m_offsets[:, None] * stride_input_m + input_n_offsets[None, :] * stride_input_n
-    res_offsets = output_m_offsets[:, None] * stride_output_m + output_n_offsets[None, :] * stride_output_n
+        up_offsets = input_m_offsets[:, None] * stride_input_m + (input_n_offsets[None, :] + size_n) * stride_input_n
+        gate_offsets = input_m_offsets[:, None] * stride_input_m + input_n_offsets[None, :] * stride_input_n
+        res_offsets = output_m_offsets[:, None] * stride_output_m + output_n_offsets[None, :] * stride_output_n
 
-    up = tl.load(
-        input_ptr + up_offsets,
-        mask=(input_n_offsets < size_n)[None, :] * (input_m_offsets < size_m)[:, None],
-        other=0.0,
-    )
-    gate = tl.load(
-        input_ptr + gate_offsets,
-        mask=(input_n_offsets < size_n)[None, :] * (input_m_offsets < size_m)[:, None],
-        other=0.0,
-    ).to(tl.float32)
+        up = tl.load(
+            input_ptr + up_offsets,
+            mask=(input_n_offsets < size_n)[None, :] * (input_m_offsets < size_m)[:, None],
+            other=0.0,
+        )
+        gate = tl.load(
+            input_ptr + gate_offsets,
+            mask=(input_n_offsets < size_n)[None, :] * (input_m_offsets < size_m)[:, None],
+            other=0.0,
+        ).to(tl.float32)
 
-    gate = gate / (1 + tl.exp(-gate))
-    gate = gate.to(tl.float16)
+        gate = gate / (1 + tl.exp(-gate))
+        gate = gate.to(tl.float16)
 
-    tl.store(
-        input_ptr + res_offsets,
-        up * gate,
-        mask=(output_n_offsets < size_n)[None, :] * (output_m_offsets < size_m)[:, None],
-    )
+        tl.store(
+            input_ptr + res_offsets,
+            up * gate,
+            mask=(output_n_offsets < size_n)[None, :] * (output_m_offsets < size_m)[:, None],
+        )
 
-
-def silu_and_mul_fwd(input):
-    if triton.__version__ >= "2.1.0":
+    def silu_and_mul_fwd(input):
         stride_input_m = input.stride(0)
         stride_input_n = input.stride(1)
         stride_output_m = input.stride(0)
@@ -78,12 +77,14 @@ def silu_and_mul_fwd(input):
             BLOCK_N,
         )
         return input[:, 0 : (input.shape[-1] // 2)]
-    elif triton.__version__ == "2.0.0":
+
+elif triton.__version__ == "2.0.0":
+
+    def torch_silu_and_mul(input: torch.Tensor):
+        return torch.nn.functional.silu(input[:, 0 : (input.shape[-1] // 2)]) * input[:, (input.shape[-1] // 2) :]
+
+    def silu_and_mul_fwd(input):
         return torch_silu_and_mul(input)
-
-
-def torch_silu_and_mul(input: torch.Tensor):
-    return torch.nn.functional.silu(input[:, 0 : (input.shape[-1] // 2)]) * input[:, (input.shape[-1] // 2) :]
 
 
 def test_silu_and_mul(M, N, dtype, device="cuda"):
