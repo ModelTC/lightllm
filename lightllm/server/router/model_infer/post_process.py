115d362d9c1a2e0765a09ba843ce3f5e11ba1d2b
wangzaijun <wangzaijun@sensetime.com>
2024-03-21 15:45:52 +0800

    fix bug

--- lightllm/server/router/model_infer/post_process.py
+++ lightllm/server/router/model_infer/post_process.py
@@ -4,31 +4,58 @@ from typing import List, Tuple
 from lightllm.server.router.model_infer.infer_batch import InferBatch
 from lightllm.common.basemodel.triton_kernel.apply_penalty import apply_penalty
 
+
 def sample(logits, reqs, eos_id=2):
     logits = logits.contiguous()
-    presence_penalties, frequency_penalties, repetition_penalties, exponential_decay_length_penalties, temperatures, top_ps, top_ks, p_token_ids, p_token_counts, p_cumsum_seq_len, p_max_len_in_batch, length_penalty_idx = _get_post_sample_tensors(reqs)
+    (
+        presence_penalties,
+        frequency_penalties,
+        repetition_penalties,
+        exponential_decay_length_penalties,
+        temperatures,
+        top_ps,
+        top_ks,
+        p_token_ids,
+        p_token_counts,
+        p_cumsum_seq_len,
+        p_max_len_in_batch,
+        length_penalty_idx,
+    ) = _get_post_sample_tensors(reqs)
 
-    apply_penalty(logits, presence_penalties, frequency_penalties, repetition_penalties, p_token_ids, p_token_counts, p_cumsum_seq_len, p_max_len_in_batch) 
-    logits[:, eos_id] = logits[:, eos_id] + torch.abs(logits[:, eos_id]) * (torch.pow(exponential_decay_length_penalties, length_penalty_idx).view((-1, 1)) - 1)
+    apply_penalty(
+        logits,
+        presence_penalties,
+        frequency_penalties,
+        repetition_penalties,
+        p_token_ids,
+        p_token_counts,
+        p_cumsum_seq_len,
+        p_max_len_in_batch,
+    )
+    logits[:, eos_id] = logits[:, eos_id] + torch.abs(logits[:, eos_id]) * (
+        torch.pow(exponential_decay_length_penalties, length_penalty_idx).view((-1,)) - 1
+    )
     logits.div_(temperatures.view((-1, 1)))
     probs = torch.softmax(logits, dim=-1)
     probs_sort, probs_idx = _top_p_top_k(probs, top_ps, top_ks)
     sampled_index = torch.multinomial(probs_sort, num_samples=1, replacement=True)
-    
+
     batch_next_token_ids = torch.gather(probs_idx, dim=1, index=sampled_index)
     batch_next_token_probs = torch.gather(probs_sort, dim=1, index=sampled_index)
-    
+
     return batch_next_token_ids.view(-1), batch_next_token_probs.view(-1)
 
-def _top_p_top_k(probs: torch.Tensor, top_ps: torch.Tensor, top_ks: torch.Tensor):    
+
+def _top_p_top_k(probs: torch.Tensor, top_ps: torch.Tensor, top_ks: torch.Tensor):
     probs_sort, probs_idx = probs.sort(dim=-1, descending=True)
-    
+
     probs_sum = torch.cumsum(probs_sort, dim=-1)
     probs_sort[(probs_sum - probs_sort) > top_ps.view(-1, 1)] = 0.0
 
     probs_sort[torch.arange(0, probs.shape[-1], device="cuda").view(1, -1) >= top_ks.view(-1, 1)] = 0.0
 
     return probs_sort, probs_idx
+
 
 def _get_post_sample_tensors(reqs):
     presence_penalties: List[float] = []
@@ -40,7 +67,9 @@ def _get_post_sample_tensors(reqs):
     top_ks: List[int] = []
     p_token_ids: List[int] = []
     p_token_counts: List[int] = []
-    p_seq_len: List[int] = [0,]
+    p_seq_len: List[int] = [
+        0,
+    ]
     p_max_len_in_batch: int = 0
     length_penalty_idx: List[int] = []
     for i, req_obj in enumerate(reqs):
@@ -50,22 +79,26 @@ def _get_post_sample_tensors(reqs):
         frequency_penalties.append(sample_param.frequency_penalty)
         repetition_penalties.append(sample_param.repetition_penalty)
         exponential_decay_length_penalties.append(sample_param.exponential_decay_length_penalty[1])
-        length_penalty_idx.append(max(len(req_obj.input_token_ids) - req_obj.prompt_len - sample_param.exponential_decay_length_penalty[0], 0))
+        length_penalty_idx.append(
+            max(len(req_obj.input_token_ids) - req_obj.prompt_len - sample_param.exponential_decay_length_penalty[0], 0)
+        )
 
         temperatures.append(sample_param.temperature)
         top_ps.append(sample_param.top_p)
         top_ks.append(sample_param.top_k)
-        
+
         for token_id, count in id_to_count.items():
             p_token_ids.append(token_id)
             p_token_counts.append(count)
         p_seq_len.append(len(id_to_count))
         p_max_len_in_batch = max(p_max_len_in_batch, len(id_to_count))
-    
+
     presence_penalties = torch.tensor(presence_penalties, dtype=torch.float, device="cuda")
     frequency_penalties = torch.tensor(frequency_penalties, dtype=torch.float, device="cuda")
     repetition_penalties = torch.tensor(repetition_penalties, dtype=torch.float, device="cuda")
-    exponential_decay_length_penalties = torch.tensor(exponential_decay_length_penalties, dtype=torch.float, device="cuda")
+    exponential_decay_length_penalties = torch.tensor(
+        exponential_decay_length_penalties, dtype=torch.float, device="cuda"
+    )
     temperatures = torch.tensor(temperatures, dtype=torch.float, device="cuda")
     top_ps = torch.tensor(top_ps, dtype=torch.float, device="cuda")
     top_ks = torch.tensor(top_ks, dtype=torch.int32, device="cuda")
@@ -74,4 +107,17 @@ def _get_post_sample_tensors(reqs):
     p_seq_len = torch.tensor(p_seq_len, dtype=torch.int32, device="cuda")
     p_cumsum_seq_len = torch.cumsum(p_seq_len, dim=0, dtype=torch.int32)
     length_penalty_idx = torch.tensor(length_penalty_idx, dtype=torch.int32, device="cuda")
-    return presence_penalties, frequency_penalties, repetition_penalties, exponential_decay_length_penalties, temperatures, top_ps, top_ks, p_token_ids, p_token_counts, p_cumsum_seq_len, p_max_len_in_batch, length_penalty_idx
+    return (
+        presence_penalties,
+        frequency_penalties,
+        repetition_penalties,
+        exponential_decay_length_penalties,
+        temperatures,
+        top_ps,
+        top_ks,
+        p_token_ids,
+        p_token_counts,
+        p_cumsum_seq_len,
+        p_max_len_in_batch,
+        length_penalty_idx,
+    )
