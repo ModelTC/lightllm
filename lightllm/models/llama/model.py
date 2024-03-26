80452e5d2bad50921e156e01e3734e6c555336a9
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2024-03-26 15:06:20 +0800

    Update model.py To Fix ntk length bug.

--- lightllm/models/llama/model.py
+++ lightllm/models/llama/model.py
@@ -144,7 +144,7 @@ class LlamaTpPartModel(TpPartBaseModel):
             scaling_factor = 1.0
         else:
             scaling_factor = self.config.get("rope_scaling", {}).get("factor", 1.0)
-        max_seq_len = self.max_seq_length
+        max_seq_len = max(self.max_seq_length, max_position_embeddings)
         self._cos_cached = torch.zeros((max_seq_len, partial_head_dim // 2), dtype=torch.float16, device="cuda")
         self._sin_cached = torch.zeros((max_seq_len, partial_head_dim // 2), dtype=torch.float16, device="cuda")
         
