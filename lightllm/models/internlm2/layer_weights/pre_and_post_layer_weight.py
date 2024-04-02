93b6c2f76b81340a0e757fa18c1edfaa338b675e
liuliang1 <liuliang1@sensetime.com>
2024-04-02 16:46:53 +0800

    Fix interlm2 post weight load, vocab_size % world_size != 0

--- lightllm/models/internlm2/layer_weights/pre_and_post_layer_weight.py
+++ lightllm/models/internlm2/layer_weights/pre_and_post_layer_weight.py
@@ -10,14 +10,14 @@ class Internlm2PreAndPostLayerWeight(LlamaPreAndPostLayerWeight):
 
     def load_hf_weights(self, weights):
         vob_size = self.network_config_["vocab_size"]
-        split_vob_size = vob_size // self.world_size_
+        split_indexes = np.linspace(0, vob_size, self.world_size_ + 1, dtype=np.int64)
+        split_start = split_indexes[self.tp_rank_]
+        split_end = split_indexes[self.tp_rank_ + 1]
         n_embed = self.network_config_["hidden_size"]
         if "model.tok_embeddings.weight" in weights:
-            self.wte_weight_ = self._cuda(weights['model.tok_embeddings.weight'][split_vob_size *
-                                                                    self.tp_rank_: split_vob_size * (self.tp_rank_ + 1), :])
+            self.wte_weight_ = self._cuda(weights['model.tok_embeddings.weight'][split_start:split_end, :])
         if 'output.weight' in weights:
-            self.lm_head_weight_ = self._cuda(weights['output.weight'][split_vob_size * self.tp_rank_: split_vob_size *
-                                                            (self.tp_rank_ + 1), :])
+            self.lm_head_weight_ = self._cuda(weights['output.weight'][split_start:split_end, :])
         if 'model.norm.weight' in weights:
             self.final_norm_weight_ = self._cuda(weights['model.norm.weight'])
 
