2aa77599acb48ed72d119a397272175431f6e62d
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2024-04-10 22:28:05 +0800

    fix

--- lightllm/models/baichuan2_7b/layer_weights/pre_and_post_layer_weight.py
+++ lightllm/models/baichuan2_7b/layer_weights/pre_and_post_layer_weight.py
@@ -22,7 +22,7 @@ class Baichuan2_7bPreAndPostLayerWeight(PreAndPostLayerWeight):
         if 'lm_head.weight' in weights:
             # print(weights['lm_head.weight'].shape)
             self.lm_head_weight_ = self._cuda(
-                 nn.functional.normalize(weights['lm_head.weight']))[split_vob_size * self.tp_rank_:split_vob_size * (self.tp_rank_ + 1), :]
+                 nn.functional.normalize(weights['lm_head.weight'])[split_vob_size * self.tp_rank_:split_vob_size * (self.tp_rank_ + 1), :])
         if 'model.norm.weight' in weights:
             self.final_norm_weight_ = self._cuda(weights['model.norm.weight'])
 
