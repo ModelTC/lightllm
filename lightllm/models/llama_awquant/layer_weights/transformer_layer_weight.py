afa677c3df8d69bd2a246409657d4a745ba195a9
helloyongyang <yongyang1030@163.com>
2024-06-02 15:39:57 +0800

    Add triton w8a8

--- lightllm/models/llama_awquant/layer_weights/transformer_layer_weight.py
+++ lightllm/models/llama_awquant/layer_weights/transformer_layer_weight.py
@@ -6,6 +6,7 @@ from functools import partial
 
 from lightllm.common.basemodel import TransformerLayerWeight
 from lightllm.common.basemodel.cuda_kernel.ppl_awquant import dynamic_channelwise_quant_fp16_i8_ppl
+from lightllm.common.basemodel.triton_kernel.quantize_gemm_int8 import quantize_int8
 
 
 class LlamaTransformerLayerActivationWeightQuantized(TransformerLayerWeight):
@@ -14,7 +15,9 @@ class LlamaTransformerLayerActivationWeightQuantized(TransformerLayerWeight):
         self.init_quant_mode()
 
     def init_quant_mode(self):
-        if "ppl_w8a8" in self.mode:
+        if "triton_w8a8" in self.mode:
+            self.quantize_weight = partial(quantize_int8, tp_rank=self.tp_rank_)
+        elif "ppl_w8a8" in self.mode:
             self.quantize_weight = partial(dynamic_channelwise_quant_fp16_i8_ppl, tp_rank=self.tp_rank_)
         else:
             raise Exception(f"error mode {self.mode}")
@@ -32,8 +35,8 @@ class LlamaTransformerLayerActivationWeightQuantized(TransformerLayerWeight):
             self.v_weight_,
             self.o_weight_,
             self.ffn_norm_weight_,
-            self.gate_proj,
-            self.up_proj,
+            # self.gate_proj,
+            # self.up_proj,
             self.down_proj,
         ]
         for i in range(len(weights)):
@@ -75,7 +78,12 @@ class LlamaTransformerLayerActivationWeightQuantized(TransformerLayerWeight):
         if f"model.layers.{self.layer_num_}.self_attn.o_proj.weight" in weights:
             o_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.o_proj.weight"]
             o_weight_ = o_weight_[:, q_split_n_embed * self.tp_rank_ : q_split_n_embed * (self.tp_rank_ + 1)]
-            self.o_weight_ = self._cuda(o_weight_.transpose(0, 1))
+            if "triton_w8a8" in self.mode:
+                self.o_weight_ = self.quantize_weight(o_weight_.transpose(0, 1).to(self.data_type_))
+            elif "ppl_w8a8" in self.mode:
+                self.o_weight_ = self._cuda(
+                    o_weight_.transpose(0, 1)
+                )  # ppl cuda kernel is slower than torch kernel in o-layer
         return
 
     def _load_ffn_weights(self, weights):
@@ -91,13 +99,22 @@ class LlamaTransformerLayerActivationWeightQuantized(TransformerLayerWeight):
             up_proj = weights[f"model.layers.{self.layer_num_}.mlp.up_proj.weight"][
                 split_inter_size * self.tp_rank_ : split_inter_size * (self.tp_rank_ + 1), :
             ]
-            self.up_proj = self.quantize_weight(up_proj.transpose(0, 1).to(self.data_type_))
+            if "triton_w8a8" in self.mode:
+                self.up_proj = up_proj.transpose(0, 1)
+            elif "ppl_w8a8" in self.mode:
+                self.up_proj = self.quantize_weight(up_proj.transpose(0, 1).to(self.data_type_))
 
         if f"model.layers.{self.layer_num_}.mlp.gate_proj.weight" in weights:
             gate_proj = weights[f"model.layers.{self.layer_num_}.mlp.gate_proj.weight"][
                 split_inter_size * self.tp_rank_ : split_inter_size * (self.tp_rank_ + 1), :
             ]
-            self.gate_proj = self.quantize_weight(gate_proj.transpose(0, 1).to(self.data_type_))
+            if "triton_w8a8" in self.mode:
+                self.gate_proj = gate_proj.transpose(0, 1)
+            elif "ppl_w8a8" in self.mode:
+                self.gate_proj = self.quantize_weight(gate_proj.transpose(0, 1).to(self.data_type_))
+
+        if "triton_w8a8" in self.mode:
+            self._try_cat_to(["gate_proj", "up_proj"], "gate_up_proj", cat_dim=1, handle_func=self.quantize_weight)
 
         if f"model.layers.{self.layer_num_}.mlp.down_proj.weight" in weights:
             down_proj = weights[f"model.layers.{self.layer_num_}.mlp.down_proj.weight"][
