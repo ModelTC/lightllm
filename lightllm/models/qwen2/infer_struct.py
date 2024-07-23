b2c976a443410e975aaf06ebf0e3420836654c44
Chen Junyi <chenjunyi@sensetime.com>
2024-07-23 17:21:25 +0800

    fix

---
+++ lightllm/models/qwen2/infer_struct.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+from lightllm.common.basemodel import InferStateInfo
+from lightllm.models.mistral.triton_kernel.init_att_sliding_window_info import init_att_window_info_fwd
+from lightllm.common.req_manager import ReqManager
+
+
+class Qwen2InferStateInfo(InferStateInfo):
+    def __init__(self):
+        super().__init__()
+        self.sliding_window = None
+        self.b_att_seq_len = None
+        self.b_att_start_loc = None
+        self.total_cache_num = None
+        # self.window_postion = None
+
+    def init_some_extra_state(self, model, input_ids: torch.Tensor):
+        self.sliding_window = model.config["sliding_window"]
+        if self.is_prefill:
+            b_seq_len_numpy = self.b_seq_len.cpu().numpy()
+            position_ids = torch.from_numpy(
+                np.concatenate([np.arange(0, b_seq_len_numpy[i]) for i in range(len(b_seq_len_numpy))], axis=0)
+            ).cuda()
+            self.position_cos = torch.index_select(model._cos_cached, 0, position_ids).view(position_ids.shape[0], -1)
+            self.position_sin = torch.index_select(model._sin_cached, 0, position_ids).view(position_ids.shape[0], -1)
+            position_ids = None
+        else:
+            position_ids = self.b_seq_len - 1
+            self.position_cos = torch.index_select(model._cos_cached, 0, position_ids).view(self.b_seq_len.shape[0], -1)
+            self.position_sin = torch.index_select(model._sin_cached, 0, position_ids).view(self.b_seq_len.shape[0], -1)
+            self.other_kv_index = self.req_manager.req_to_token_indexs[self.b_req_idx[0], 0].item()
+
+            self.b_att_seq_len = torch.zeros_like(self.b_seq_len)
+            init_att_window_info_fwd(self.batch_size, self.b_seq_len, self.b_att_seq_len, self.sliding_window)
+            self.b_att_start_loc = torch.cumsum(self.b_att_seq_len, 0) - self.b_att_seq_len
+            self.total_cache_num = torch.sum(self.b_att_seq_len).item()
+        return
--- test/test_server.py
+++ test/test_server.py
@@ -19,20 +19,30 @@ class RequestThread(threading.Thread):
             print('Error:', response.status_code, response.text)
 
 
-url = 'http://localhost:8000/generate'
+url = 'http://localhost:8888/generate'
 headers = {'Content-Type': 'application/json'}
 
 for i in range(1):
     data = {
-        'inputs': 'San Francisco is a',
-        # 'temperature': 0.1,
-        'parameters' : {
-            'do_sample': False,
+        "inputs": """<|im_start|>system\nYou are a helpful assistant.<|im_end|>\n<|im_start|>user\nWhat is the correct answer to this question: trans-cinnamaldehyde was treated with methylmagnesium bromide, forming product 1.\n\n1 was treated with pyridinium chlorochromate, forming product 2.\n\n3 was treated with (dimethyl(oxo)-l6-sulfaneylidene)methane in DMSO at elevated temperature, forming product 3.\n\nhow many carbon atoms are there in product 3?\nChoices:\n(A)12\n(B)14\n(C)11\n(D)10\nFormat your response as follows: "The correct answer is (insert answer here)"<|im_end|>\n<|im_start|>assistant\n""",
+        "parameters": {
+            "temperature": 1,
+            "max_new_tokens": 300,
+            "stop_sequences": [
+            "<|endofblock|>",
+            "<|endofblock|><|im_end|>",
+            "<|endoftext|>",
+            "<|im_start|>"
+            ],
+            # "repetition_penalty": 1.05,
+            "top_k": 1,
+            "best_of": 1,
+            "do_sample": True
         }
     }
     thread = RequestThread(url, headers, data)
     thread.start()
-
+"""
 time.sleep(2)
 
 for i in range(20):
@@ -46,3 +56,4 @@ for i in range(20):
     }
     thread = RequestThread(url, headers, data)
     thread.start()
+"""
