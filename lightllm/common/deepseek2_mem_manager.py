03abc2b05682dbcebcd4694dd6191e97e4fac8e5
wangzaijun <wangzaijun@sensetime.com>
2024-07-25 13:32:12 +0800

    fix deepseekv2 mem manager for cpu cache move

--- lightllm/common/deepseek2_mem_manager.py
+++ lightllm/common/deepseek2_mem_manager.py
@@ -5,4 +5,7 @@ from .mem_manager import MemoryManager
 
 class Deepseek2MemoryManager(MemoryManager):
     def _init_buffers(self, size, dtype, head_num, head_dim, layer_num):
-        self.kv_buffer = [torch.empty((size, head_num, head_dim), dtype=dtype, device="cuda") for _ in range(layer_num)]
+        self.kv_buffer = [
+            torch.empty((size, head_num, head_dim), dtype=dtype, pin_memory=self.pin_memory, device=self.device_type)
+            for _ in range(layer_num)
+        ]
