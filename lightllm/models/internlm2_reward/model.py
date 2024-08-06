5d4d9d3d4298d0b30698b69aada6974eec67f36e
JYILY <1411045825@qq.com>
2024-08-06 17:44:03 +0800

    add tp support for reward model

--- lightllm/models/internlm2_reward/model.py
+++ lightllm/models/internlm2_reward/model.py
@@ -2,6 +2,7 @@ import os
 import json
 import torch
 
+from lightllm.models.internlm2_reward.layer_infer.post_layer_infer import Internlm2RewardPostLayerInfer
 from lightllm.models.internlm2_reward.layer_weights.pre_and_post_layer_weight import (
     Internlm2RewardPreAndPostLayerWeight,
 )
@@ -11,6 +12,8 @@ from lightllm.models.internlm2.model import Internlm2TpPartModel
 class Internlm2RewardTpPartModel(Internlm2TpPartModel):
     # weight class
     pre_and_post_weight_class = Internlm2RewardPreAndPostLayerWeight
+
+    post_layer_infer_class = Internlm2RewardPostLayerInfer
 
     def __init__(self, kvargs):
         super().__init__(kvargs)
