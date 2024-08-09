1b25c6de570ed9377f4cbaa3887429ea7f105c60
WANDY666 <1060304770@qq.com>
2024-08-09 11:14:29 +0800

    delete debug info

--- lightllm/models/deepseek2/layer_weights/transformer_layer_weight.py
+++ lightllm/models/deepseek2/layer_weights/transformer_layer_weight.py
@@ -126,7 +126,6 @@ class Deepseek2TransformerLayerWeight(TransformerLayerWeight):
         if f"model.layers.{self.layer_num_}.self_attn.o_proj.weight" in weights:
             o_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.o_proj.weight"]
             o_weight_ = o_weight_.T.view(self.num_attention_heads, self.qk_nope_head_dim, -1)
-            print("Here", self.tp_q_head_num_, self.world_size_, self.network_config_["num_attention_heads"])
             self.o_weight_ = (
                 o_weight_[self.tp_q_head_num_ * self.tp_rank_ : self.tp_q_head_num_ * (self.tp_rank_ + 1), :, :]
                 .contiguous()
