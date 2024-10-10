d89be84ca44cdbef3ded8321158515ce6cb5a2a7
sufubao <1411045825@qq.com>
2024-10-10 19:21:13 +0800

    fix new embedding operator

--- lightllm/models/starcoder/layer_infer/pre_layer_infer.py
+++ lightllm/models/starcoder/layer_infer/pre_layer_infer.py
@@ -30,7 +30,6 @@ class StarcoderPreLayerInfer(PreLayerInfer):
         embedding(input_ids, layer_weight.wte_weight_, self.vob_start_id_, self.vob_end_id_, input_embdings)
         if self.world_size_ > 1:
             dist.all_reduce(input_embdings, op=dist.ReduceOp.SUM, async_op=False)
-        input_embdings = self._norm(input_embdings, infer_state, layer_weight)
 
         position_embeds = self.alloc_tensor((infer_state.position_ids.shape[0], layer_weight.wpe_weight_.shape[1]), data_type=layer_weight.data_type_)
         embedding(infer_state.position_ids, layer_weight.wpe_weight_, 0, layer_weight.wpe_weight_.shape[0], position_embeds)
