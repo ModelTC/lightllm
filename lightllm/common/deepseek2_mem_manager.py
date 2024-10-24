0aebaecc5832473a0780d53591257dfb2ca320a4
shihaobai <42648726+shihaobai@users.noreply.github.com>
2024-10-24 13:41:15 +0800

    fix deepseek

--- lightllm/common/deepseek2_mem_manager.py
+++ lightllm/common/deepseek2_mem_manager.py
@@ -4,5 +4,9 @@ from .mem_manager import MemoryManager
 
 
 class Deepseek2MemoryManager(MemoryManager):
+
+    def get_cell_size(self):
+        return self.head_num * self.head_dim * self.layer_num * torch._utils._element_size(self.dtype)
+
     def _init_buffers(self, size, dtype, head_num, head_dim, layer_num):
         self.kv_buffer = [torch.empty((size, head_num, head_dim), dtype=dtype, device="cuda") for _ in range(layer_num)]
