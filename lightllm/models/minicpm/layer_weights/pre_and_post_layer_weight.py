49dfb51a6cb1f551b44370dadbea11ac64a476cc
baishihao <baishihao@sensetime.com>
2024-11-14 14:00:08 +0800

    refactor minicpm

--- lightllm/models/minicpm/layer_weights/pre_and_post_layer_weight.py
+++ lightllm/models/minicpm/layer_weights/pre_and_post_layer_weight.py
@@ -18,10 +18,8 @@ class MiniCPMPreAndPostLayerWeight(LlamaPreAndPostLayerWeight):
         split_start = split_indexes[self.tp_rank_]
         split_end = split_indexes[self.tp_rank_ + 1]
         if "model.embed_tokens.weight" in weights:
-            # print(weights['model.embed_tokens.weight'].shape)
             self.wte_weight_ = self._cuda(weights["model.embed_tokens.weight"][split_start:split_end, :])
         if "lm_head.weight" in weights:
-            # print(weights['lm_head.weight'].shape)
             self.lm_head_weight_ = self._cuda(weights["lm_head.weight"][split_start:split_end, :]) / self.lm_head_scale
         if "model.norm.weight" in weights:
             self.final_norm_weight_ = self._cuda(weights["model.norm.weight"])
@@ -30,7 +28,7 @@ class MiniCPMPreAndPostLayerWeight(LlamaPreAndPostLayerWeight):
 
     def verify_load(self):
         if not hasattr(self, "lm_head_weight_"):
-            self.lm_head_weight_ =  self.wte_weight_ / self.lm_head_scale
+            self.lm_head_weight_ = self.wte_weight_ / self.lm_head_scale
         self.wte_weight_ = self.wte_weight_ * self.scale_emb
         errors = "weights load not ok"
         weights = [self.wte_weight_, self.lm_head_weight_, self.final_norm_weight_]
