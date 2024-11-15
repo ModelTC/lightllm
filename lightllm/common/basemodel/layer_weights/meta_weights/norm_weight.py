2dcb77b55e7c72c439bcc16ff88ec9b438d15dc3
sufubao <1411045825@qq.com>
2024-11-15 07:15:12 +0000

    [Refactor][model] refactor cohere model

--- lightllm/common/basemodel/layer_weights/meta_weights/norm_weight.py
+++ lightllm/common/basemodel/layer_weights/meta_weights/norm_weight.py
@@ -5,3 +5,18 @@ from .base_weight import BaseWeightTpl
 class NormWeight(BaseWeightTpl):
     def __init__(self, weight_name, data_type, bias_name=None):
         super().__init__(weight_name, data_type, bias_name)
+
+
+class TpNormWeight(BaseWeightTpl):
+    def __init__(self, weight_name, data_type, split_n_embed, bias_name=None):
+        super().__init__(weight_name, data_type, bias_name)
+        self.split_n_embed = split_n_embed
+
+    def load_hf_weights(self, weights):
+        start = self.offset + self.split_n_embed * self.tp_rank_
+        end = self.offset + self.split_n_embed * (self.tp_rank_ + 1)
+
+        if self.weight_name in weights:
+            self.weight = weights[self.weight_name][start:end].to(self.data_type_).cuda(self.tp_rank_)
+        if self.bias_name in weights:
+            self.bias = weights[self.bias_name][start:end].to(self.data_type_).cuda(self.tp_rank_)
--- lightllm/models/cohere/layer_weights/transformer_layer_weight.py
+++ lightllm/models/cohere/layer_weights/transformer_layer_weight.py
@@ -1,107 +1,31 @@
 from lightllm.models.llama.layer_weights.transformer_layer_weight import LlamaTransformerLayerWeight
+from lightllm.common.basemodel.layer_weights.meta_weights import (
+    BaseWeight,
+    ROWMMWeight,
+    COLMMWeight,
+    NormWeight,
+    TpNormWeight,
+)
 
 
 class CohereTransformerLayerWeight(LlamaTransformerLayerWeight):
     def __init__(self, layer_num, tp_rank, world_size, data_type, network_config, mode=[]):
-        super().__init__(layer_num, tp_rank, world_size, data_type, network_config, mode)
         self.use_qk_norm = network_config.get("use_qk_norm", False)
+        super().__init__(layer_num, tp_rank, world_size, data_type, network_config, mode)
         return
 
-    def load_hf_weights(self, weights):
-        self._load_qkvo_weights(weights)
-        self._load_ffn_weights(weights)
-        return
-
-    def verify_load(self):
-        errors = "weights load not ok"
-        weights = [
-            self.att_norm_weight_,
-            self.q_weight_,
-            self.kv_weight_,
-            self.o_weight_,
-            self.gate_up_proj,
-            self.down_proj,
-        ]
-        for i in range(len(weights)):
-            assert weights[i] is not None, "index:" + str(i) + " " + errors
-        if self.use_qk_norm:
-            qk_weights = [self.q_norm_weight_, self.k_norm_weight_]
-            for i in range(len(qk_weights)):
-                assert qk_weights[i] is not None, "index:" + str(i + len(weights)) + " " + errors
-        return
-
-    def _load_qkvo_weights(self, weights):
-        # input layernorm params
-        if f"model.layers.{self.layer_num_}.input_layernorm.weight" in weights:
-            self.att_norm_weight_ = self._cuda(weights[f"model.layers.{self.layer_num_}.input_layernorm.weight"])
-
-        n_embed = self.network_config_["hidden_size"]
-        q_split_n_embed = n_embed // self.world_size_
-        kv_split_n_embed = (
-            n_embed
-            // self.network_config_["num_attention_heads"]
-            * self.network_config_["num_key_value_heads"]
-            // self.world_size_
-        )
+    def init_norm(self, weights):
         q_split_head = self.network_config_["num_attention_heads"] // self.world_size_
-        kv_split_head = self.network_config_["num_key_value_heads"] // self.world_size_
+        k_split_head = self.network_config_["num_key_value_heads"] // self.world_size_
 
-        # q k v weights for llama
-        if f"model.layers.{self.layer_num_}.self_attn.q_proj.weight" in weights:
-            self.q_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.q_proj.weight"]
-            self.q_weight_ = self.q_weight_[q_split_n_embed * self.tp_rank_ : q_split_n_embed * (self.tp_rank_ + 1), :]
-            self.q_weight_ = self._cuda(self.q_weight_.transpose(0, 1))
+        self.att_norm_weight_ = NormWeight(f"model.layers.{self.layer_num_}.input_layernorm.weight", self.data_type_)
 
-        if f"model.layers.{self.layer_num_}.self_attn.k_proj.weight" in weights:
-            k_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.k_proj.weight"]
-            k_weight_ = k_weight_[kv_split_n_embed * self.tp_rank_ : kv_split_n_embed * (self.tp_rank_ + 1), :]
-            self.k_weight_ = k_weight_.transpose(0, 1)
+        if self.use_qk_norm:
+            self.q_norm_weight_ = TpNormWeight(
+                f"model.layers.{self.layer_num_}.self_attn.q_norm.weight", self.data_type_, q_split_head
+            )
+            self.k_norm_weight_ = TpNormWeight(
+                f"model.layers.{self.layer_num_}.self_attn.k_norm.weight", self.data_type_, k_split_head
+            )
 
-        if f"model.layers.{self.layer_num_}.self_attn.v_proj.weight" in weights:
-            v_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.v_proj.weight"]
-            v_weight_ = v_weight_[kv_split_n_embed * self.tp_rank_ : kv_split_n_embed * (self.tp_rank_ + 1), :]
-            self.v_weight_ = v_weight_.transpose(0, 1)
-
-        if f"model.layers.{self.layer_num_}.self_attn.q_norm.weight" in weights:
-            q_norm_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.q_norm.weight"]
-            q_norm_weight_ = q_norm_weight_[q_split_head * self.tp_rank_ : q_split_head * (self.tp_rank_ + 1)]
-            self.q_norm_weight_ = self._cuda(q_norm_weight_)
-        if f"model.layers.{self.layer_num_}.self_attn.k_norm.weight" in weights:
-            k_norm_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.k_norm.weight"]
-            k_norm_weight_ = k_norm_weight_[kv_split_head * self.tp_rank_ : kv_split_head * (self.tp_rank_ + 1)]
-            self.k_norm_weight_ = self._cuda(k_norm_weight_)
-
-        # attention output dense params
-        if f"model.layers.{self.layer_num_}.self_attn.o_proj.weight" in weights:
-            self.o_weight_ = weights[f"model.layers.{self.layer_num_}.self_attn.o_proj.weight"]
-            self.o_weight_ = self.o_weight_[:, q_split_n_embed * self.tp_rank_ : q_split_n_embed * (self.tp_rank_ + 1)]
-            self.o_weight_ = self._cuda(self.o_weight_.transpose(0, 1))
-
-        self._try_cat_to(["k_weight_", "v_weight_"], "kv_weight_", cat_dim=1)
-
-        return
-
-    def _load_ffn_weights(self, weights):
-        inter_size = self.network_config_["intermediate_size"]
-        split_inter_size = inter_size // self.world_size_
-
-        if f"model.layers.{self.layer_num_}.mlp.up_proj.weight" in weights:
-            up_proj = weights[f"model.layers.{self.layer_num_}.mlp.up_proj.weight"][
-                split_inter_size * self.tp_rank_ : split_inter_size * (self.tp_rank_ + 1), :
-            ]
-            self.up_proj = up_proj.transpose(0, 1)
-
-        if f"model.layers.{self.layer_num_}.mlp.gate_proj.weight" in weights:
-            gate_proj = weights[f"model.layers.{self.layer_num_}.mlp.gate_proj.weight"][
-                split_inter_size * self.tp_rank_ : split_inter_size * (self.tp_rank_ + 1), :
-            ]
-            self.gate_proj = gate_proj.transpose(0, 1)
-
-        self._try_cat_to(["gate_proj", "up_proj"], "gate_up_proj", cat_dim=1)
-
-        if f"model.layers.{self.layer_num_}.mlp.down_proj.weight" in weights:
-            self.down_proj = weights[f"model.layers.{self.layer_num_}.mlp.down_proj.weight"][
-                :, split_inter_size * self.tp_rank_ : split_inter_size * (self.tp_rank_ + 1)
-            ]
-            self.down_proj = self._cuda(self.down_proj.transpose(0, 1))
         return
