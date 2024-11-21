7c9b2091f956b8aebb679da84d5f0fa17d0f63f5
baishihao <baishihao@sensetime.com>
2024-11-21 15:12:51 +0800

    update internlm2

--- lightllm/models/internlm2/layer_weights/transformer_layer_weight.py
+++ lightllm/models/internlm2/layer_weights/transformer_layer_weight.py
@@ -12,7 +12,7 @@ class Internlm2TransformerLayerWeight(LlamaTransformerLayerWeight):
         return
 
     def load_hf_weights(self, weights):
-        qkv_weight_name = f"{self.layer_name}.attention.wqkv.weight"
+        qkv_weight_name = f"model.layers.{self.layer_num_}.attention.wqkv.weight"
         if qkv_weight_name in weights:
             qkv_weight_ = weights[qkv_weight_name]
             q_groups = self.n_head // self.n_kv_head
@@ -20,19 +20,18 @@ class Internlm2TransformerLayerWeight(LlamaTransformerLayerWeight):
             q_weight_ = qkv_weight_[:, :q_groups, :, :].reshape(-1, qkv_weight_.shape[-1])
             k_weight_ = qkv_weight_[:, -2, :, :].reshape(-1, qkv_weight_.shape[-1])
             v_weight_ = qkv_weight_[:, -1, :, :].reshape(-1, qkv_weight_.shape[-1])
-            weights[self._q_bias_name] = q_weight_
-            weights[self._k_bias_name] = k_weight_
-            weights[self._v_bias_name] = v_weight_
+            weights[self._q_weight_name] = q_weight_
+            weights[self._k_weight_name] = k_weight_
+            weights[self._v_weight_name] = v_weight_
             del weights[qkv_weight_name]
         super().load_hf_weights(weights)
 
     def _init_weight_names(self):
         super()._init_weight_names()
-        self._o_weight_name = f"{self.layer_name}.attention.wo.weight"
-        self._o_weight_name = f"{self.layer_name}.attention.wo.bias"
-        
-        self._gate_weight_name = f"{self.layer_name}.feed_forward.w1.weight"
-        self._up_weight_name = f"{self.layer_name}.feed_forward.w3.weight"
-        self._down_weight_name = f"{self.layer_name}.feed_forward.w2.weight"
-        self.att_norm_weight_name = f"{self.layer_name}.attention_norm.weight"
-        self.ffn_norm_weight_name = f"{self.layer_name}.ffn_norm.weight"
+        self._o_weight_name = f"model.layers.{self.layer_num_}.attention.wo.weight"
+
+        self._gate_weight_name = f"model.layers.{self.layer_num_}.feed_forward.w1.weight"
+        self._up_weight_name = f"model.layers.{self.layer_num_}.feed_forward.w3.weight"
+        self._down_weight_name = f"model.layers.{self.layer_num_}.feed_forward.w2.weight"
+        self.att_norm_weight_name = f"model.layers.{self.layer_num_}.attention_norm.weight"
+        self.ffn_norm_weight_name = f"model.layers.{self.layer_num_}.ffn_norm.weight"
