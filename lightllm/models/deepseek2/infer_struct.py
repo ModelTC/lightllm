c9decdbb4ba6a8296ad4284ab35591ec57340e7b
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2024-12-09 10:44:08 +0800

    fix

--- lightllm/models/deepseek2/infer_struct.py
+++ lightllm/models/deepseek2/infer_struct.py
@@ -10,5 +10,7 @@ class Deepseek2InferStateInfo(LlamaInferStateInfo):
 
     def init_some_extra_state(self, model, input_ids: torch.Tensor):
         super().init_some_extra_state(model, input_ids)
-        self.kv_starts = torch.cat([self.b_start_loc, self.b_start_loc[-1:] + self.b_seq_len[-1:]], dim=0)
+        # 只有 decode 阶段使用 ppl 的优化算子才会有这个管理变量
+        if not self.is_prefill:
+            self.kv_starts = torch.cat([self.b_start_loc, self.b_start_loc[-1:] + self.b_seq_len[-1:]], dim=0)
         return
