94756cf430799309b30833d293bc9794a3e66bc1
wangzaijun <wangzaijun@sensetime.com>
2024-12-18 17:23:15 +0800

    add better moe kernel

--- lightllm/common/fused_moe/test_better_fused_moe.py
+++ lightllm/common/fused_moe/test_better_fused_moe.py
@@ -3,35 +3,44 @@ from .better_fused_moe import moe_align, moe_align1
 
 
 def test_moe_align():
+    expert_num = 5
+    token_num = 3
+    topk = 3
     topk_ids = torch.tensor([[0, 1, 2], [0, 3, 1], [3, 1, 4]], dtype=torch.int32, device="cuda")
-    out = torch.zeros((5, 3), dtype=torch.int32, device="cuda")
+    out = torch.zeros((expert_num, token_num * topk), dtype=torch.int32, device="cuda")
     out.fill_(0)
     moe_align(topk_ids, out)
-    true = torch.tensor([[1, 1, 0], [1, 1, 1], [1, 0, 0], [0, 1, 1], [0, 0, 1]], dtype=torch.int32, device="cuda")
+    true = torch.tensor(
+        [
+            [1, 0, 0, 1, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 1, 0, 1, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 1, 0, 1, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 1],
+        ],
+        dtype=torch.int32,
+        device="cuda",
+    )
     assert torch.equal(out, true)
 
 
 def test_moe_align1():
     experts_info = torch.tensor(
-        [
-            [1, 0, 1, 0, 1],
-            [0, 1, 0, 1, 0],
-            [1, 1, 0, 1, 0],
-        ],
+        [[1, 0, 0, 0], [0, 1, 1, 0], [0, 0, 0, 1], [0, 0, 0, 0]],
         dtype=torch.int32,
         device="cuda",
     )
-    experts_token_num = torch.zeros((3,), dtype=torch.int32, device="cuda")
+    experts_token_num = torch.zeros((4,), dtype=torch.int32, device="cuda")
 
-    moe_align1(experts_info, experts_token_num)
+    moe_align1(experts_info, experts_token_num, 2)
 
-    true_experts_token_num = torch.tensor([3, 2, 3], device="cuda", dtype=torch.int32)
+    true_experts_token_num = torch.tensor([1, 2, 1, 0], device="cuda", dtype=torch.int32)
     true_experts_info = torch.tensor(
-        [[0, 2, 4, 0, 1], [1, 3, 0, 1, 0], [0, 1, 3, 1, 0]], device="cuda:0", dtype=torch.int32
+        [[0, 0, 0, 0], [1, 2, 1, 0], [3, 0, 0, 1], [0, 0, 0, 0]], device="cuda:0", dtype=torch.int32
     )
 
-    torch.equal(experts_token_num, true_experts_token_num)
-    torch.equal(experts_info, true_experts_info)
+    assert torch.equal(experts_token_num, true_experts_token_num)
+    assert torch.equal(experts_info, true_experts_info)
 
 
 if __name__ == "__main__":
