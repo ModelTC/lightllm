4352845fc7490fef81f26854fe11075c2aed2103
wangzaijun <wangzaijun@sensetime.com>
2024-12-22 11:21:46 +0800

    add grid count auto calcu.

--- lightllm/utils/device_utils.py
+++ lightllm/utils/device_utils.py
@@ -17,3 +17,39 @@ def get_current_device_id():
         return int(device_id)
     else:
         raise RuntimeError("Torch CUDA is not avaliable.")
+
+
+@lru_cache(maxsize=None)
+def get_device_sm_count():
+    import triton
+    from triton.runtime import driver
+
+    properties = driver.active.utils.get_device_properties(0)
+    return properties["multiprocessor_count"]
+
+
+@lru_cache(maxsize=None)
+def get_device_sm_regs_num():
+    import triton
+    from triton.runtime import driver
+
+    properties = driver.active.utils.get_device_properties(0)
+    return properties["max_num_regs"]
+
+
+@lru_cache(maxsize=None)
+def get_device_sm_shared_mem_num():
+    import triton
+    from triton.runtime import driver
+
+    properties = driver.active.utils.get_device_properties(0)
+    return properties["max_shared_mem"]
+
+
+@lru_cache(maxsize=None)
+def get_device_warp_size():
+    import triton
+    from triton.runtime import driver
+
+    properties = driver.active.utils.get_device_properties(0)
+    return properties["warpSize"]
