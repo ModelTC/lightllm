2f45df5499d6efaa1b2fce5dd433bb9dd6ff22d4
shihaobai <baishihao@sensetime.com>
2024-12-23 14:03:49 +0800

    get custom get_image_path

--- lightllm/models/vit/__init__.py
+++ lightllm/models/vit/__init__.py
@@ -2,6 +2,7 @@ import os
 import importlib.util
 from lightllm.utils.log_utils import init_logger
 from lightllm.models.internvl.img_process import load_image as default_load_image
+from lightllm.models.internvl.img_process import get_image_patch as default_get_image_patch
 
 logger = init_logger(__name__)
 
@@ -23,3 +24,22 @@ def get_load_image_func(weight_dir):
         logger.info(f"pre_process.py not found in {weight_dir}, using default load_image.")
 
     return default_load_image
+
+
+def get_image_patch_func(weight_dir):
+    global load_image
+    pre_process_path = os.path.join(weight_dir, "pre_process.py")
+    if os.path.exists(pre_process_path):
+        logger.info(f"Found pre_process.py in {weight_dir}, attempting to load get_image_patch from it.")
+        spec = importlib.util.spec_from_file_location("pre_process", pre_process_path)
+        pre_process = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(pre_process)
+        if hasattr(pre_process, "get_image_patch"):
+            logger.info("load_image function replaced by the one in pre_process.py.")
+            return pre_process.get_image_patch
+        else:
+            logger.info("get_image_patch function not found in pre_process.py.")
+    else:
+        logger.info(f"pre_process.py not found in {weight_dir}, using default get_image_patch.")
+
+    return default_get_image_patch
