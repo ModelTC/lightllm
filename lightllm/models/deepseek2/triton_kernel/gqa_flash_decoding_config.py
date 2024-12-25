2f9eda38da43a48ba68009ce9848e2f25955ec92
wangzaijun <wangzaijun@sensetime.com>
2024-12-25 12:21:05 +0800

    fix

---
+++ lightllm/models/deepseek2/triton_kernel/gqa_flash_decoding_config.py
@@ -0,0 +1,62 @@
+from lightllm.common.kernel_config import KernelConfigs
+from frozendict import frozendict
+from functools import lru_cache
+from typing import Dict
+
+
+class MlaDecodeAttentionKernelConfig(KernelConfigs):
+    kernel_name: str = "mla_decode_attentnion"
+
+    @classmethod
+    @lru_cache(maxsize=200)
+    def try_to_get_best_config(
+        cls,
+        batch_size: int,
+        total_token_num: int,
+        q_head_num: int,
+        q_head_dim: int,
+        q_rope_dim: int,
+        out_dtype: str,
+    ) -> dict:
+        key_params = {
+            "q_head_num": q_head_num,
+            "q_head_dim": q_head_dim,
+            "q_rope_dim": q_rope_dim,
+            "out_dtype": out_dtype,
+        }
+        key_params = frozendict(key_params)
+
+        finded_config = cls.get_the_config(key_params)
+
+        if finded_config:
+            # two search dim, first is total_token_num, second is batch_size
+            batch_size_config: dict = finded_config[min(finded_config.keys(), key=lambda x: abs(x - total_token_num))]
+            config = batch_size_config[min(batch_size_config.keys(), key=lambda x: abs(x - batch_size))]
+
+            return config
+        else:
+            config = {
+                "BLOCK_SEQ": 64,
+                "BLOCK_N": 16,
+                "BLOCK_Q_HEAD": 16,
+                "stage1_num_warps": 4,
+                "stage1_num_stages": 2,
+                "stage2_num_warps": 4,
+                "stage2_num_stages": 2,
+            }
+        return config
+
+    @classmethod
+    def save_config(
+        cls, q_head_num: int, q_head_dim: int, q_rope_dim: int, out_dtype: str, config_json: Dict[int, Dict[int, Dict]]
+    ):
+
+        key_params = {
+            "q_head_num": q_head_num,
+            "q_head_dim": q_head_dim,
+            "q_rope_dim": q_rope_dim,
+            "out_dtype": out_dtype,
+        }
+        key_params = frozendict(key_params)
+
+        return cls.store_config(key_params, config_json)
