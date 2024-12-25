7cc0111d7d8c67930eb222b9fc7a6afbe48ab841
wangzaijun <wangzaijun@sensetime.com>
2024-12-25 10:31:31 +0800

    all config store to one dir, and add store interface.

--- lightllm/common/fused_moe/moe_kernel_configs.py
+++ lightllm/common/fused_moe/moe_kernel_configs.py
@@ -8,6 +8,8 @@ logger = init_logger(__name__)
 
 
 class MoeGroupedGemmKernelConfig(KernelConfigs):
+    kernel_name: str = "grouped_moe_gemm_kernel"
+
     @classmethod
     @lru_cache(maxsize=200)
     def try_to_get_best_config(
@@ -32,7 +34,7 @@ class MoeGroupedGemmKernelConfig(KernelConfigs):
         }
         key_params = frozendict(key_params)
 
-        finded_config = KernelConfigs.get_the_config(key_params, os.path.dirname(os.path.realpath(__file__)))
+        finded_config = cls.get_the_config(key_params)
 
         if finded_config:
             config = finded_config[min(finded_config.keys(), key=lambda x: abs(x - M))]
@@ -57,3 +59,28 @@ class MoeGroupedGemmKernelConfig(KernelConfigs):
                     "num_stages": 1,
                 }
         return config
+
+    @classmethod
+    def save_config(
+        cls,
+        N: int,
+        K: int,
+        topk_num: int,
+        expert_num: int,
+        mul_routed_weight: bool,
+        use_fp8_w8a8: bool,
+        out_dtype: str,
+        config_json: dict,
+    ):
+        key_params = {
+            "N": N,
+            "K": K,
+            "topk_num": topk_num,
+            "expert_num": expert_num,
+            "mul_routed_weight": mul_routed_weight,
+            "use_fp8_w8a8": use_fp8_w8a8,
+            "out_dtype": out_dtype,
+        }
+        key_params = frozendict(key_params)
+
+        return cls.store_config(key_params, config_json)
