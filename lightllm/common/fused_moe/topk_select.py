c56ef595dd9666c4b6f16f3931e3cb68e34bd4c7
wangzaijun <wangzaijun@sensetime.com>
2024-12-26 17:00:53 +0800

    fix

--- lightllm/common/fused_moe/topk_select.py
+++ lightllm/common/fused_moe/topk_select.py
@@ -1,3 +1,22 @@
+# Adapted from
+# https://github.com/vllm-project/vllm/blob/v0.6.4.post1/vllm/model_executor/layers/fused_moe/fused_moe.py
+# of the vllm-project/vllm GitHub repository.
+#
+# Copyright 2023 ModelTC Team
+# Copyright 2023 vLLM Team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import torch
 from lightllm.common.vllm_kernel import _custom_ops as ops
 
