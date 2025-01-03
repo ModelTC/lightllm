c987c0834b9737b2b441cbf0fe57262b71430d74
fanyunqian <fanyunqian@sensetime.com>
2025-01-03 09:23:30 +0800

    refactor: split into different files

---
+++ lightllm/models/llama/triton_kernel/vsm_gqa_flash_decoding.py
@@ -0,0 +1,99 @@
+import torch
+from .vsm_gqa_flash_decoding_config import VSMGQADecodeAttentionKernelConfig
+from .vsm_gqa_flash_decoding_stage1 import vsm_gqa_flash_decoding_stage1
+from .vsm_gqa_flash_decoding_stage2 import vsm_gqa_flash_decoding_stage2
+
+def vsm_gqa_flash_decoding(
+    q, 
+    infer_state,
+    k, 
+    v,
+    total_token_in_the_batch,
+    q_head_dim,
+    q_head_num,
+    kv_head_dim,
+    out=None,
+    alloc_tensor_func=torch.empty,
+    **run_config: VSMGQADecodeAttentionKernelConfig
+):
+    batch_size = infer_state.batch_size
+    max_len_in_batch = infer_state.max_len_in_batch
+    total_token_in_the_batch = infer_state.total_token_num
+
+    q_shape = (batch_size, q_head_num, q_head_dim)
+
+    if not run_config:
+        if torch.cuda.is_current_stream_capturing():
+            avg_seq_len_in_batch = max_len_in_batch
+        else:
+            avg_seq_len_in_batch = total_token_in_the_batch // batch_size
+        run_config = VSMGQADecodeAttentionKernelConfig.try_to_get_best_config(
+            batch_size=batch_size,
+            avg_seq_len_in_batch=avg_seq_len_in_batch,
+            q_head_num=q_head_num,
+            q_head_dim=q_head_dim,
+            kv_head_dim=kv_head_dim,
+            out_dtype=torch.bfloat16,
+        )
+
+    o_tensor = alloc_tensor_func(q.shape, q.dtype, q.device) if out is None else out
+    
+    # virtual calculate
+    mid_o_block_seq = torch.empty([1], dtype=torch.int64, device="cuda")
+    mid_o_batch_start_index = alloc_tensor_func(
+        [
+            batch_size,
+        ],
+        dtype=torch.int64,
+        device="cuda",
+    )
+    mid_o = torch.empty([q_head_num, 0, kv_head_dim], dtype=torch.float32, device="cuda")
+    mid_o_logexpsum = torch.empty([q_head_num, 0], dtype=torch.float32, device="cuda")
+    vsm_count = vsm_gqa_flash_decoding_stage1(
+        q.view(q_shape),
+        k,
+        v,
+        infer_state.req_manager.req_to_token_indexs,
+        infer_state.b_req_idx,
+        infer_state.b_seq_len,
+        total_token_in_the_batch,
+        mid_o,
+        mid_o_logexpsum,
+        mid_o_block_seq,
+        mid_o_batch_start_index,
+        get_sm_count=True,
+        **run_config
+    )
+
+    # real calculate
+    mid_o = torch.empty([q_head_num, vsm_count, kv_head_dim], dtype=torch.float32, device="cuda")
+    mid_o_logexpsum = torch.empty([q_head_num, vsm_count], dtype=torch.float32, device="cuda")
+    vsm_gqa_flash_decoding_stage1(
+        q.view(q_shape),
+        k,
+        v,
+        infer_state.req_manager.req_to_token_indexs,
+        infer_state.b_req_idx,
+        infer_state.b_seq_len,
+        total_token_in_the_batch,
+        mid_o,
+        mid_o_logexpsum,
+        mid_o_block_seq,
+        mid_o_batch_start_index,
+        get_sm_count=False,
+        **run_config
+    )
+
+    vsm_gqa_flash_decoding_stage2(
+        mid_o,
+        mid_o_logexpsum,
+        mid_o_block_seq,
+        mid_o_batch_start_index,
+        infer_state.b_seq_len,
+        o_tensor.view(q_shape),
+        **run_config
+    )
+    return o_tensor
+
+
+    
---
+++ lightllm/models/llama/triton_kernel/vsm_gqa_flash_decoding_stage2.py
@@ -0,0 +1,85 @@
+import triton 
+import triton.language as tl
+from .vsm_gqa_flash_decoding_config import VSMGQADecodeAttentionKernelConfig
+
+@triton.jit
+def _fwd_kernel_vsm_gqa_flash_decoding_stage2(
+    mid_o, 
+    mid_o_logexp_sum,
+    mid_o_chunk_num,
+    mid_o_batch_start,
+    b_seq_len,
+    chunk_size_ptr,
+    out,
+    stride_mid_o_h,
+    stride_mid_o_s,
+    stride_mid_o_d,
+    stride_mid_o_logexpsum_h,
+    stride_mid_o_logexpsum_s,
+    stride_out_bs,
+    stride_out_h,
+    stride_out_d,
+    Q_DIM: tl.constexpr,
+    NUM_STAGES: tl.constexpr,
+):
+    cur_head = tl.program_id(0)
+    cur_batch = tl.program_id(1)
+
+    off_d = tl.arange(0, Q_DIM)
+    cur_batch_seq_len = tl.load(b_seq_len + cur_batch)
+    cur_chunk_start = tl.load(mid_o_batch_start + cur_batch)
+    cur_chunk_num = tl.load(mid_o_chunk_num)
+    chunk_size = tl.load(chunk_size_ptr)
+
+    sum_exp = 0.
+    max_term = -float('inf')
+    acc = tl.zeros([Q_DIM], dtype=tl.float32)
+    
+    
+    for cur_chunk in tl.range(0, cur_chunk_num, 1, num_stages=NUM_STAGES):
+        off_m = cur_head * stride_mid_o_h + (cur_chunk_start + cur_chunk) * stride_mid_o_s + off_d
+        off_ml = cur_head * stride_mid_o_logexpsum_h + (cur_chunk_start + cur_chunk)
+        cur_mid = tl.load(mid_o + off_m)
+        cur_ml = tl.load(mid_o_logexp_sum + off_ml)
+        new_max = tl.maximum(cur_ml, max_term)
+
+        old_scale = tl.exp(max_term - new_max)
+        acc *= old_scale
+        exp_ml = tl.exp(cur_ml - new_max)
+        acc += exp_ml * cur_mid
+        sum_exp = sum_exp * old_scale + exp_ml
+        max_term = new_max
+    tl.store(out + cur_batch + stride_out_bs + cur_head + stride_out_h + off_d, acc / sum_exp)
+    
+
+def vsm_gqa_flash_decoding_stage2(
+    mid_o,
+    mid_o_logexpsum,
+    mid_o_chunk_num,
+    mid_o_batch_start_index,
+    b_seq_len,
+    chunk_size,
+    o,
+    **run_config: VSMGQADecodeAttentionKernelConfig
+):
+    num_warps = run_config['stage2_num_warps']
+    num_stages = run_config['stage2_num_stages']
+
+    Q_DIM = mid_o.shape[-1]
+    batch_size, head_num = mid_o_batch_start_index.shape[0], mid_o.shape[0]
+    grid = (head_num, batch_size)
+
+    _fwd_kernel_vsm_gqa_flash_decoding_stage2[grid](
+        mid_o,
+        mid_o_logexpsum,
+        mid_o_chunk_num,
+        mid_o_batch_start_index,
+        b_seq_len,
+        chunk_size,
+        o,
+        *mid_o.stride(),
+        *mid_o_logexpsum.stride(),
+        *o.stride(),
+        Q_DIM=Q_DIM,
+        NUM_STAGES=num_stages
+    )
