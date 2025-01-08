28f40ace8aa5b6cd8da26dd080f59fd73ccc4be8
wangzaijun <wangzaijun@sensetime.com>
2025-01-08 10:51:39 +0800

    添加 args 参数。

--- lightllm/utils/envs_utils.py
+++ lightllm/utils/envs_utils.py
@@ -1,4 +1,6 @@
 import os
+import json
+from easydict import EasyDict
 from functools import lru_cache
 from lightllm.utils.log_utils import init_logger
 
@@ -9,3 +11,16 @@ logger = init_logger(__name__)
 def get_unique_server_name():
     service_uni_name = os.getenv("UNIQUE_SERVICE_NAME_ID")
     return service_uni_name
+
+
+def set_env_start_args(args):
+    args_dict = vars(args)
+    os.environ["LIGHTLLM_START_ARGS"] = json.dumps(args_dict)
+    return
+
+
+@lru_cache(maxsize=None)
+def get_env_start_args():
+    start_args = json.loads(os.environ["LIGHTLLM_START_ARGS"])
+    start_args = EasyDict(start_args)
+    return start_args
