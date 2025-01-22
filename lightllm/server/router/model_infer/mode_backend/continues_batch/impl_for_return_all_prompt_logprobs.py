fix return all prompt logprobs

lightllm/server/router/model_infer/mode_backend/continues_batch/impl_for_return_all_prompt_logprobs.py
@@ -1,7 +1,8 @@
 import torch
 from .impl import ContinuesBatchBackend
+from typing import List, Tuple
 from lightllm.utils.infer_utils import calculate_time, mark_start, mark_end
-from lightllm.server.router.model_infer.infer_batch import InferReq, InferSamplingParams
+from lightllm.server.router.model_infer.infer_batch import InferReq, InferSamplingParams, g_infer_context
 from .pre_process import prepare_prefill_inputs
 from .post_process import sample
 
@@ -10,13 +11,12 @@ class ReturnPromptLogProbBackend(ContinuesBatchBackend):
     def __init__(self) -> None:
         super().__init__()
 
-    @calculate_time(show=False, min_cost_ms=300)
-    def prefill_batch(self, batch_id):
+    def prefill(self, run_reqs: List[Tuple]):
         # 在 return all_prompt_logprobs 的模式下，不能启用 dynamic prompt cache
         assert self.radix_cache is None
-        output_dict = {}
-        batch = self.cache.pop(batch_id)
-        kwargs, run_reqs = prepare_prefill_inputs(batch, self.radix_cache, self.model.mem_manager)
+        req_ids = self._init_reqs(run_reqs, init_req_obj=True)
+
+        kwargs, run_reqs = prepare_prefill_inputs(req_ids)
 
         prompt_all_logits = self.model.forward(**kwargs)
         input_ids = kwargs["input_ids"]
@@ -31,41 +31,45 @@ class ReturnPromptLogProbBackend(ContinuesBatchBackend):
 
         b_start_loc = b_start_loc.cpu().numpy()
         b_seq_len = b_seq_len.cpu().numpy()
+
+        finished_req_ids = []
         for req_obj, next_token_id, next_token_logprob, start_loc, seq_len in zip(
             run_reqs, next_token_ids, next_token_logprobs, b_start_loc, b_seq_len
         ):
             # prefill and decode is same
-            req_obj.cur_kv_len = len(req_obj.input_token_ids)
-            req_obj.input_token_ids.append(next_token_id)
-            req_obj.out_token_id_count[next_token_id] += 1
-            metadata = {
-                "id": int(next_token_id),
-                "logprob": float(next_token_logprob),
-            }
+            req_obj: InferReq = req_obj
+            req_obj.cur_kv_len = req_obj.get_cur_total_len()
 
+            req_obj.set_next_gen_token_id(next_token_id, next_token_logprob)
+            req_obj.cur_output_len += 1
+
+            # 填充 logprobs 信息
             cur_ids: torch.Tensor = input_ids[start_loc : start_loc + seq_len]
             cur_logits = prompt_all_logits[start_loc : start_loc + seq_len]
             cur_logprobs = torch.log_softmax(cur_logits, dim=-1, dtype=torch.float)[0:-1, :]
             cur_logprobs = torch.gather(cur_logprobs, dim=1, index=cur_ids[1:].view(-1, 1)).detach().cpu().numpy()
 
-            cur_ids = cur_ids.cpu().numpy()
-            all_prompts = []
-            for index in range(len(cur_ids) - 1):
-                tmp_dict = {int(cur_ids[index + 1]): float(cur_logprobs[index, 0])}
-                all_prompts.append([int(cur_ids[index]), tmp_dict])
+            for i in range(req_obj.shm_req.input_len - 1):
+                req_obj.shm_req.shm_logprobs.arr[i + 1] = cur_logprobs[i]
 
+            req_obj.out_token_id_count[next_token_id] += 1
             req_obj.update_finish_status(self.eos_id)
 
-            metadata["prompt_logprobs"] = all_prompts
-            metadata["prompt_token_ids"] = [int(e) for e in cur_ids]
-            output_dict[req_obj.r_id] = (
-                req_obj.req_status,
-                req_obj.cur_kv_len,
-                req_obj.get_output_len(),
-                [(int(next_token_id), metadata)],
-                req_obj.finish_status.value,
-                None,
-            )  # 请求状态， 当前占用的kv的长度， 当前输出token的数量， 输出的token的id和元信息列表， 是否推理结束的状态， 额外保留参数
+            if req_obj.finish_status.is_finished() or req_obj.shm_req.router_aborted:
+                finished_req_ids.append(req_obj.shm_req.request_id)
 
-        self.cache[batch.batch_id] = batch
-        return output_dict
+            if self.tp_rank < self.dp_size:
+                # shm_cur_kv_len shm_cur_output_len 是 router 调度进程需要读的信息
+                # finish_token_index finish_status candetoken_out_len 是
+                # detokenization 进程需要的信息，注意这些变量的写入顺序避免异步协同问题。
+                req_obj.shm_req.shm_cur_kv_len = req_obj.cur_kv_len
+                req_obj.shm_req.shm_cur_output_len = req_obj.cur_output_len
+
+                if req_obj.finish_status.is_finished():
+                    req_obj.shm_req.finish_token_index = req_obj.get_cur_total_len() - 1
+                    req_obj.shm_req.finish_status = req_obj.finish_status
+
+                req_obj.shm_req.candetoken_out_len = req_obj.cur_output_len
+
+        g_infer_context.filter(finished_request_ids=finished_req_ids)
+        return