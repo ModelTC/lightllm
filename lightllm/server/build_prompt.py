9138726e7077f84ceee68b9dcd2b4008f5e04429
baishihao <baishihao@sensetime.com>
2025-01-23 14:07:28 +0800

    fix openai stream & update chat template

--- lightllm/server/build_prompt.py
+++ lightllm/server/build_prompt.py
@@ -11,5 +11,10 @@ def init_tokenizer(args):
 async def build_prompt(request) -> str:
     global tokenizer
     messages = request.messages
-    input_str = tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+    kwargs = {"conversation": messages}
+    if request.character_settings:
+        kwargs["character_settings"] = request.character_settings
+    if request.role_settings:
+        kwargs["role_setting"] = request.role_settings
+    input_str = tokenizer.apply_chat_template(**kwargs, tokenize=False, add_generation_prompt=True)
     return input_str
