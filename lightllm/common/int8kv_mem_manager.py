180030d3848399a553da8485a52421ff2966df17
helloyongyang <yongyang1030@163.com>
2025-02-12 13:17:20 +0800

    fix prefix noquant bug

--- lightllm/common/int8kv_mem_manager.py
+++ lightllm/common/int8kv_mem_manager.py
@@ -14,11 +14,16 @@ class INT8KVMemoryManager(MemoryManager):
         ) + 2 * self.head_num * self.layer_num * torch._utils._element_size(self.dtype)
 
     def _init_buffers(self, size, dtype, head_num, head_dim, layer_num):
-        self.kv_buffer = [
-            torch.empty((size, 2 * head_num, head_dim), dtype=torch.int8, device="cuda") for _ in range(layer_num)
-        ]
-        self.scale_buffer = [torch.empty((size, 2 * head_num, 1), dtype=dtype, device="cuda") for _ in range(layer_num)]
+        self.kv_buffer = torch.empty((layer_num, size, 2 * head_num, head_dim), dtype=torch.int8, device="cuda")
+        self.scale_buffer = torch.empty((layer_num, size, 2 * head_num, 1), dtype=dtype, device="cuda")
 
     def _free_buffers(self):
         self.kv_buffer = None
         self.scale_buffer = None
+
+    def get_index_kv_buffer(self, index):
+        return {"kv_buffer": self.kv_buffer[:, index], "scale_buffer": self.scale_buffer[:, index]}
+
+    def load_index_kv_buffer(self, index, load_tensor_dict):
+        self.kv_buffer[:, index].copy_(load_tensor_dict["kv_buffer"])
+        self.scale_buffer[:, index].copy_(load_tensor_dict["scale_buffer"])
--- lightllm/common/ppl_int4kv_mem_manager.py
+++ lightllm/common/ppl_int4kv_mem_manager.py
@@ -17,14 +17,18 @@ class PPLINT4KVMemoryManager(MemoryManager):
         )
 
     def _init_buffers(self, size, dtype, head_num, head_dim, layer_num):
-        self.kv_buffer = [
-            torch.empty((size, 2 * head_num, head_dim // 2), dtype=torch.int8, device="cuda") for _ in range(layer_num)
-        ]
-        self.scale_buffer = [
-            torch.empty((size, 2 * head_num, head_dim // self.group_quant_size), dtype=dtype, device="cuda")
-            for _ in range(layer_num)
-        ]
+        self.kv_buffer = torch.empty((layer_num, size, 2 * head_num, head_dim // 2), dtype=torch.int8, device="cuda")
+        self.scale_buffer = torch.empty(
+            (layer_num, size, 2 * head_num, head_dim // self.group_quant_size), dtype=dtype, device="cuda"
+        )
 
     def _free_buffers(self):
         self.kv_buffer = None
         self.scale_buffer = None
+
+    def get_index_kv_buffer(self, index):
+        return {"kv_buffer": self.kv_buffer[:, index], "scale_buffer": self.scale_buffer[:, index]}
+
+    def load_index_kv_buffer(self, index, load_tensor_dict):
+        self.kv_buffer[:, index].copy_(load_tensor_dict["kv_buffer"])
+        self.scale_buffer[:, index].copy_(load_tensor_dict["scale_buffer"])
