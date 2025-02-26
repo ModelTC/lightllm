4e419dfe56c007ab4be6da255348145b43dc0ce1
wangzaijun <wzjhelloworld@qq.com>
2025-02-26 15:35:48 +0800

    修改 rank 配置。

--- lightllm/utils/dist_utils.py
+++ lightllm/utils/dist_utils.py
@@ -15,11 +15,13 @@ def get_environ(environ_name):
 
 
 def _init_distributed_env(kvargs):
-    set_global_rank(kvargs["rank_id"])  # TODO: implement DP
+    set_global_rank(kvargs["rank_id"])
     set_global_world_size(kvargs["world_size"])
-    set_dp_world_size(kvargs.get("dp_size", 1))
-    # set_current_dp_rank()
-    set_current_tp_rank(kvargs["rank_id"])
+    set_dp_size(kvargs.get("dp_size", 1))
+    set_dp_world_size(get_global_world_size() // get_dp_size())
+    set_current_dp_rank(get_global_rank() // get_dp_world_size())
+    set_current_dp_inner_rank(get_global_rank() % get_dp_world_size())
+    
     size_per_node = (kvargs["world_size"] + kvargs["args"].nnodes - 1) // kvargs["args"].nnodes
     local_tp_rank = kvargs["rank_id"] - size_per_node * kvargs["args"].node_rank
     set_current_device_id(local_tp_rank)
@@ -36,47 +38,47 @@ def _init_distributed_env(kvargs):
         dist.all_reduce(_a)
         del _a
 
-
 def set_global_rank(global_rank: int):
     set_environ("GLOBAL_RANK", global_rank)
-
 
 def get_global_rank():
     return int(get_environ("GLOBAL_RANK"))
 
-
 def set_global_world_size(world_size: int):
     set_environ("GLOBAL_WORLD_SIZE", world_size)
-
 
 def get_global_world_size():
     return int(get_environ("GLOBAL_WORLD_SIZE"))
 
+def set_dp_size(dp_size:int):
+    """
+    total dp num
+    """
+    set_environ("LIGHTLLM_DP_SIZE", dp_size)
+    
+def get_dp_size():
+    return get_environ("LIGHTLLM_DP_SIZE")
 
 def set_dp_world_size(world_size: int):
     set_environ("DP_WORLD_SIZE", world_size)
 
-
 def get_dp_world_size():
     return int(get_environ("DP_WORLD_SIZE"))
-
 
 def set_current_dp_rank(rank: int):
     set_environ("CURRENT_DP_RANK", rank)
 
-
 def get_current_dp_rank():
-    assert False, "not implemented"
     return int(get_environ("CURRENT_DP_RANK"))
 
-
-def set_current_tp_rank(rank: int):
-    set_environ("CURRENT_TP_RANK", rank)
-
+def set_current_dp_inner_rank(rank: int):
+    set_environ("CURRENT_DP_INNER_RANK", rank)
+    
+def get_current_dp_inner_rank():
+    return get_environ("CURRENT_DP_INNER_RANK")
 
 def set_current_device_id(device_id: int):
     set_environ("CURRENT_DEVICE_ID", device_id)
-
 
 def get_current_device_id():
     return int(get_environ("CURRENT_DEVICE_ID"))
