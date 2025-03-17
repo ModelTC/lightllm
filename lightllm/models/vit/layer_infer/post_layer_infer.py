3b609192c371490431c557ded5fe2f2449af4795
baishihao <baishihao@sensetime.com>
2025-03-17 09:28:22 +0000

    add gelt and rms_norm

--- lightllm/models/vit/layer_infer/post_layer_infer.py
+++ lightllm/models/vit/layer_infer/post_layer_infer.py
@@ -3,7 +3,7 @@ import torch.functional as F
 import torch.distributed as dist
 from lightllm.models.vit.layer_weights.pre_and_post_layer_weight import ViTPreAndPostLayerWeight
 from lightllm.utils.dist_utils import get_current_rank_in_dp, get_dp_world_size
-
+from lightllm.models.vit.triton_kernel.gelu_vit import gelu
 
 class ViTPostLayerInfer:
     """ """
@@ -44,8 +44,9 @@ class ViTPostLayerInfer:
             layer_weight.mlp1_1_bias_, vit_embeds_norm.view(-1, vit_embeds_norm.shape[-1]), layer_weight.mlp1_1_weight_
         )
 
-        vit_embeds_gelu = torch.nn.functional.gelu(vit_embeds_1)
-
+        # vit_embeds_gelu = torch.nn.functional.gelu(vit_embeds_1)
+        vit_embeds_gelu = gelu(vit_embeds_1)
+        
         vit_embeds_out = torch.addmm(
             layer_weight.mlp1_3_bias_,
             vit_embeds_gelu.view(-1, self.llm_hidden_size // self.tp_world_size_),
