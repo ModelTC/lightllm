2c649ed0fe9336b356e3d7866b328c129968eba2
wufeiyang <jayfeather9@qq.com>
2025-03-19 17:12:10 +0800

    add hiradix cache implement untested

--- lightllm/server/router/dynamic_prompt/hiradix_cache.py
+++ lightllm/server/router/dynamic_prompt/hiradix_cache.py
@@ -10,55 +10,32 @@ class HiRadixCache(RadixCache):
         super().__init__(unique_name, total_token_num, rank_in_node, mem_manager)
         self.cache_controller = cache_controller
     
-    def _insert_helper(self, node: TreeNode, key, value):
-        if node.is_leaf():
-            self.evict_tree_set.discard(node)
+    def insert(self, key, value=None):
+        if value is None:
+            value = key
 
-        try:
-            first_key_id = key[0].item()
-            if first_key_id in node.children.keys():
-                child: TreeNode = node.children[first_key_id]
-                prefix_len = match(key, child.token_id_key)
-                if prefix_len == len(key):
-                    if child.is_leaf():
-                        self.evict_tree_set.discard(child)
-                    child.update_time()
-                    if child.is_leaf():
-                        self.evict_tree_set.add(child)
-                    return prefix_len
-
-                elif prefix_len < len(key) and prefix_len < len(child.token_id_key):
-                    if child.is_leaf():
-                        self.evict_tree_set.discard(child)
-
-                    key = key[prefix_len:]
-                    value = value[prefix_len:]
-                    split_parent_node = child.split_node(prefix_len)
-                    new_node = split_parent_node.add_and_return_new_child(key, value)
-                    # update total token num
-                    self.tree_total_tokens_num.arr[0] += len(new_node.token_mem_index_value)
-
-                    if split_parent_node.is_leaf():
-                        self.evict_tree_set.add(split_parent_node)
-                    if new_node.is_leaf():
-                        self.evict_tree_set.add(new_node)
-
-                    if child.is_leaf():
-                        self.evict_tree_set.add(child)
-                    return prefix_len
-                elif prefix_len < len(key) and prefix_len == len(child.token_id_key):
-                    return prefix_len + self._insert_helper(child, key[prefix_len:], value[prefix_len:])
-                else:
-                    assert False, "can not run to here"
-
+        assert len(key) == len(value)  # and len(key) >= 1
+        if len(key) == 0:
+            return 0
+        
+        self.cache_controller.write(key, value)
+        return self._insert_helper(self.root_node, key, value)
+    
+    def match_prefix(self, key, update_refs=False):
+        assert len(key) != 0
+        ans_value_list = []
+        available_hi_result = self.cache_controller.readable_length(key)
+        tree_node = self._match_prefix_helper(self.root_node, key, ans_value_list, update_refs=False)
+        if tree_node == self.root_node or available_hi_result > len(ans_value_list):
+            hi_result = self.cache_controller.read(key)
+            self._insert_helper(tree_node, key, hi_result)
+        tree_node = self._match_prefix_helper(self.root_node, key, ans_value_list, update_refs=update_refs)
+        if tree_node != self.root_node:
+            if len(ans_value_list) != 0:
+                value = torch.concat(ans_value_list)
             else:
-                new_node = node.add_and_return_new_child(key, value)
-                # update total token num
-                self.tree_total_tokens_num.arr[0] += len(new_node.token_mem_index_value)
-                if new_node.is_leaf():
-                    self.evict_tree_set.add(new_node)
-                return 0
-        finally:
-            node.update_time()
-            if node.is_leaf():
-                self.evict_tree_set.add(node)
+                assert False, "can not run to here"
+            return tree_node, len(value), value
+        else:
+            self.dec_node_ref_counter(self.root_node)
+            return None, 0, None
