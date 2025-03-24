29424efc9c775e4471e2cfe54e38efb2ec871d8e
sangchengmeng <sangchengmeng@sensetime.com>
2025-03-24 06:41:46 +0000

    [add]openai_api_support_image

--- lightllm/server/api_models.py
+++ lightllm/server/api_models.py
@@ -4,11 +4,47 @@ from pydantic import BaseModel, Field, field_validator
 from typing import Dict, List, Optional, Union, Literal
 import uuid
 
+class ImageData(BaseModel):
+    type: str
+    data: str
+
+class ImageURL(BaseModel):
+    url: List[ImageData]
+
+    @field_validator("url", mode="before")
+    def ensure_list(cls, v):
+        if isinstance(v, list):
+            new_list = []
+            for item in v:
+                if isinstance(item, str):
+                    new_list.append({"type": "base64", "data": item})
+                elif isinstance(item, dict):
+                    if "type" not in item:
+                        item["type"] = "base64"
+                    new_list.append(item)
+                else:
+                    new_list.append(item)
+            return new_list
+        elif isinstance(v, str):
+            return [{"type": "base64", "data": v}]
+        elif isinstance(v, dict):
+            if "type" not in v:
+                v["type"] = "base64"
+            return [v]
+        return v
+
+class MessageContent(BaseModel):
+    type: str
+    text: Optional[str] = None
+    image_url: Optional[ImageURL] = None
+
+class Message(BaseModel):
+    role: str
+    content: Union[str, List[MessageContent]]
 
 class ChatCompletionRequest(BaseModel):
-    # The openai api native parameters
     model: str
-    messages: List[Dict[str, str]]
+    messages: List[Message]
     function_call: Optional[str] = "none"
     temperature: Optional[float] = 1
     top_p: Optional[float] = 1.0
