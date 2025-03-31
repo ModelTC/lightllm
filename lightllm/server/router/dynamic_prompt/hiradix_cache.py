7a4d8d0488eaf7de9ecff72059ae69761618a0e3
wufeiyang <jayfeather9@qq.com>
2025-03-31 09:23:30 +0800

    add write support

--- lightllm/server/router/dynamic_prompt/hiradix_cache.py
+++ lightllm/server/router/dynamic_prompt/hiradix_cache.py
@@ -1,14 +1,26 @@
 import torch
-from .cache_controller import HiCacheController
 from .radix_cache import RadixCache, TreeNode, match
 from typing import Tuple, Dict, Set, List
 from lightllm.common.mem_manager import MemoryManager
+from lightllm.utils.log_utils import init_logger
+from threading import Lock, Thread
 
+logger = init_logger(__name__)
 
 class HiRadixCache(RadixCache):
-    def __init__(self, cache_controller: HiCacheController, unique_name, total_token_num, rank_in_node, mem_manager: MemoryManager = None):
+    def __init__(self, unique_name, total_token_num, rank_in_node, mem_manager, max_seq_length, py_cache_service):
         super().__init__(unique_name, total_token_num, rank_in_node, mem_manager)
-        self.cache_controller = cache_controller
+        try:
+            assert py_cache_service is not None
+            self.py_cache_service = py_cache_service
+            self.hi_cache_buffer_len = 0
+            self.hi_cache_key_buffer = torch.empty(max_seq_length, dtype=torch.int64, device="cpu")
+            self.hi_cache_kv_buffer = self.mem_manager.alloc(max_seq_length)
+            self.moving = False
+            self.moving_lock = Lock()
+        except Exception as e:
+            logger.error(f"error alloc hi cache buffer {e}, fallback to normal radix cache")
+            self.hi_cache_kv_buffer = None
     
     def insert(self, key, value=None):
         if value is None:
@@ -18,9 +30,39 @@ class HiRadixCache(RadixCache):
         if len(key) == 0:
             return 0
         
-        self.cache_controller.write(key, value)
+        # current implement is serial, TODO: make it parallel
+        # if no hi_cache_buffer, work with normal radix cache
+        if self.hi_cache_kv_buffer is not None:
+            do_copy = False
+            # and if is moving, ignore this insert request
+            with self.moving_lock:
+                if not self.moving:
+                    self.moving = True
+                    do_copy = True
+            if do_copy:
+                # copy the key and value to the hi_cache_buffer
+                self.hi_cache_key_buffer[:len(key)].copy_(key)
+                self.hi_cache_buffer_len = len(key)
+                for buffer_index, index in enumerate(value):
+                    kv_data = self.mem_manager.get_index_kv_buffer(index)
+                    self.mem_manager.load_index_kv_buffer(self.hi_cache_kv_buffer[buffer_index], kv_data)
+                # create a new thread to store the buffer
+                thread = Thread(target=self._store_buffer)
+                thread.start()
+
         return self._insert_helper(self.root_node, key, value)
     
+    def _store_buffer(self):
+        assert self.moving
+        assert self.hi_cache_buffer_len > 0
+        assert self.hi_cache_kv_buffer is not None
+        key = self.hi_cache_key_buffer[:self.hi_cache_buffer_len].tolist()
+        write_task = self.py_cache_service.create(tokens=key, kv_page_indexer=self.hi_cache_kv_buffer[:self.hi_cache_buffer_len], mode="w")
+        while not write_task.ready(): 
+            pass
+        with self.moving_lock:
+            self.moving = False
+
     def match_prefix(self, key, update_refs=False):
         assert len(key) != 0
         ans_value_list = []
