71aeb1aa005b3d48b5dd8bcda06ad8e98fa3a29a
wufeiyang <jayfeather9@qq.com>
2025-04-15 16:56:52 +0800

    add little fixes, still buggy

--- lightllm/server/router/dynamic_prompt/hiradix_cache.py
+++ lightllm/server/router/dynamic_prompt/hiradix_cache.py
@@ -3,7 +3,7 @@ from .radix_cache import RadixCache, TreeNode, match
 from typing import Tuple, Dict, Set, List
 from lightllm.common.mem_manager import MemoryManager
 from lightllm.utils.log_utils import init_logger
-from threading import Lock, Thread
+from threading import Lock
 from cache.ffi.pywarp import PyLocalCacheService
 import time
 
@@ -13,21 +13,27 @@ class HiRadixCache(RadixCache):
     def __init__(self, unique_name, total_token_num, rank_in_node, mem_manager, max_seq_length):
         super().__init__(unique_name, total_token_num, rank_in_node, mem_manager)
         logger.info(f"Initializing HiRadixCache")
+        self.rank_in_node = rank_in_node
+        self.write_task = None
+        self.starting = True
         try:
             all_buffers = self.mem_manager.kv_buffer
             all_buffers = all_buffers.view(all_buffers.shape[0], all_buffers.shape[1], -1)
             self.py_cache_service = PyLocalCacheService(
-                file="cache/cache_file", storage_size=32 * (1024**3),
+                file="cache/cache_file", storage_size=64 * (1024**3),
                 num_shard=32, kvcache=all_buffers, num_worker=32)
             self.hi_cache_buffer_len = 0
             self.hi_cache_key_buffer = torch.empty(max_seq_length, dtype=torch.int64, device="cpu")
             self.hi_cache_kv_buffer = self.mem_manager.alloc(max_seq_length)
-            self.moving = False
+            self.start_store_task = False
             self.moving_lock = Lock()
         except Exception as e:
             logger.error(f"error alloc hi cache buffer {e}, fallback to normal radix cache")
             self.hi_cache_kv_buffer = None
     
+    # write a new function, only insert input(after prefill), call after prefill,
+    # then when the decode finishes, do syncronize to see whether this can be free
+    # no buffer, parallel insert inputs
     def insert(self, key, value=None):
         if value is None:
             value = key
@@ -42,9 +48,16 @@ class HiRadixCache(RadixCache):
             do_copy = False
             # and if is moving, ignore this insert request
             with self.moving_lock:
-                if not self.moving:
-                    self.moving = True
+                if (not self.start_store_task) and self.write_task is not None:
+                    if self.write_task.ready():
+                        logger.info(f"HiCache of [{self.rank_in_node}]: stored one kvcache with len = {self.hi_cache_buffer_len}")
+                        self.start_store_task = True # ensure ready => start new only one kvcache stores
+                        do_copy = True
+                elif self.write_task is None and self.starting:
+                    self.starting = False
+                    self.start_store_task = True
                     do_copy = True
+
             if do_copy:
                 # copy the key and value to the hi_cache_buffer
                 self.hi_cache_key_buffer[:len(key)].copy_(key)
@@ -53,29 +66,31 @@ class HiRadixCache(RadixCache):
                     kv_data = self.mem_manager.get_index_kv_buffer(index)
                     self.mem_manager.load_index_kv_buffer(self.hi_cache_kv_buffer[buffer_index], kv_data)
                 # create a new thread to store the buffer
-                thread = Thread(target=self._store_buffer)
-                thread.start()
+                self._store_buffer()
 
         return self._insert_helper(self.root_node, key, value)
     
     def _store_buffer(self):
         logger.info(f"Storing buffer size = {self.hi_cache_buffer_len}")
-        assert self.moving
         assert self.hi_cache_buffer_len > 0
         assert self.hi_cache_kv_buffer is not None
         key = self.hi_cache_key_buffer[:self.hi_cache_buffer_len].tolist()
-        write_task = self.py_cache_service.create(tokens=key, kv_page_indexer=self.hi_cache_kv_buffer[:self.hi_cache_buffer_len].type(torch.int64).cuda(), mode="w")
-        while not write_task.ready(): 
-            time.sleep(0.5)
-        logger.info(f"HiCache: stored one kvcache with len = {self.hi_cache_buffer_len}")
+        self.write_task = self.py_cache_service.create(
+            tokens=key, kv_page_indexer=self.hi_cache_kv_buffer[:self.hi_cache_buffer_len].type(torch.int64).cuda(), mode="w")
         with self.moving_lock:
-            self.moving = False
+            self.start_store_task = False
 
     def match_prefix(self, key, update_refs=False):
+        st_time = time.time()
         assert len(key) != 0
         ans_value_list = []
         tree_node = self._match_prefix_helper(self.root_node, key, ans_value_list, update_refs=update_refs)
-        max_len = self._query_hi_cache(key)
+        # add a parameter if get long enough (>50%)
+        first_query_time = time.time()
+        logger.info(f"HiCache of [{self.rank_in_node}]: No.1 First GPU query took {first_query_time - st_time}")
+        max_len = self._query_hi_cache(key) # x64
+        hi_cache_query_time = time.time()
+        logger.info(f"HiCache of [{self.rank_in_node}]: No.2 Disk query took {hi_cache_query_time - first_query_time}")
         logger.info(f"Matched {len(ans_value_list)} from gpu and {max_len} from disk.")
         pull_hi_cache = False
         if max_len > len(ans_value_list):
@@ -85,14 +100,20 @@ class HiRadixCache(RadixCache):
             except:
                 pull_hi_cache = False
         if pull_hi_cache:
-            buffers = self.mem_manager.alloc(max_len).type(torch.int64).cuda()
+            buffers = self.mem_manager.alloc(max_len).type(torch.int64).cuda() # type change & no .cuda()
             read_task = self.py_cache_service.create(tokens=key[:max_len], kv_page_indexer=buffers, mode="r")
             while not read_task.ready():
-                time.sleep(0.5)
+                time.sleep(0.01)
+            hicache_pull_time = time.time()
+            logger.info(f"HiCache of [{self.rank_in_node}]: No.3 Disk pull took {hicache_pull_time - hi_cache_query_time}")
             logger.info(f"HiCache pulled one cache with len = {max_len}")
-            self._insert_helper(self.root_node, key, buffers)
+            # maybe try: add a function to only insert middle part of kv cache
+            self._insert_helper(self.root_node, key, buffers.cpu()) # no .cpu()
+            insert_time = time.time()
+            logger.info(f"HiCache of [{self.rank_in_node}]: No.4 Reinsert took {insert_time - hicache_pull_time}")
             ans_value_list = []
             tree_node = self._match_prefix_helper(self.root_node, key, ans_value_list, update_refs=update_refs)
+            logger.info(f"HiCache of [{self.rank_in_node}]: No.5 Re match prefix took {time.time() - insert_time}")
         if tree_node != self.root_node:
             if len(ans_value_list) != 0:
                 value = torch.concat(ans_value_list)
