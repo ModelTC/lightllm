9b884071fa6c301fad40b599fd3ae1eb4995e4a8
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2025-04-15 20:42:40 +0800

    fix

--- lightllm/models/qwen2_vl/layer_infer/transformer_layer_infer.py
+++ lightllm/models/qwen2_vl/layer_infer/transformer_layer_infer.py
@@ -4,8 +4,6 @@ import torch.distributed as dist
 import numpy as np
 
 from lightllm.models.llama.layer_infer.transformer_layer_infer import LlamaTransformerLayerInfer
-
-import torch.nn as nn
 from functools import partial
 
 
@@ -26,33 +24,10 @@ def apply_multimodal_rotary_pos_emb(q, k, cos, sin, mrope_section, unsqueeze_dim
     return q_embed, k_embed
 
 
-class Qwen2RMSNorm(nn.Module):
-    def __init__(self, hidden_size, device, eps=1e-6):
-        super().__init__()
-        self.variance_epsilon = eps
-
-    def forward(self, hidden_states, weight):
-        input_dtype = hidden_states.dtype
-        hidden_states = hidden_states.to(torch.float32)
-        variance = hidden_states.pow(2).mean(-1, keepdim=True)
-        hidden_states = hidden_states * torch.rsqrt(variance + self.variance_epsilon)
-
-        return (weight * hidden_states).to(input_dtype)
-
-
 class Qwen2VLTransformerLayerInfer(LlamaTransformerLayerInfer):
     def __init__(self, layer_num, network_config, mode=[]):
         super().__init__(layer_num, network_config, mode)
         self.mrope_section = network_config["rope_scaling"]["mrope_section"]
-        self.norm_fwd = Qwen2RMSNorm(
-            network_config["hidden_size"], device="cuda", eps=network_config.get("rms_norm_eps", 1e-06)
-        )
-
-    def _bind_norm(self):
-        self._ffn_norm = partial(LlamaTransformerLayerInfer._ffn_norm, self)
-
-    def _att_norm(self, input_embedding, infer_state, layer_weight) -> torch.Tensor:
-        return self.norm_fwd(input_embedding, weight=layer_weight.att_norm_weight_.weight)
 
     def _get_qkv(self, input, cache_kv, infer_state, layer_weight):
         q = layer_weight.q_proj.mm(input)
