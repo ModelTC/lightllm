05a046974e222adf7184eac8fd4ff0f150d74753
wufeiyang <jayfeather9@qq.com>
2025-04-18 16:20:50 +0800

    Change to only store prefill

--- lightllm/server/router/model_infer/mode_backend/chunked_prefill/impl.py
+++ lightllm/server/router/model_infer/mode_backend/chunked_prefill/impl.py
@@ -59,6 +59,7 @@ class ChunkedPrefillBackend(ModeBackend):
                     prefill_reqs, is_chuncked_mode=True, is_multimodal=self.is_multimodal
                 )
                 logits = self.model.forward(**kwargs)
+                self.store_hicache_after_prefill(run_reqs)
                 self._overlap_req_init_and_filter(
                     uninit_reqs=uninit_reqs, ok_finished_reqs=ok_finished_reqs, clear_list=True
                 )
