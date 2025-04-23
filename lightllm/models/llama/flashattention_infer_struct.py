64eb930d59196eb11eb9d7ebc792d812a9a3c1b9
baishihao <baishihao@sensetime.com>
2025-04-23 21:01:24 +0800

    fa3 for llama

--- lightllm/models/llama/flashattention_infer_struct.py
+++ lightllm/models/llama/flashattention_infer_struct.py
@@ -24,8 +24,12 @@ class FlashAttentionStateInfo(LlamaInferStateInfo):
     def init_some_extra_state(self, model, input_ids: torch.Tensor):
         super().init_some_extra_state(model, input_ids)
         if self.is_prefill:
-            # TODO: Prefill Attention
-            pass
+            self.cu_seqlens_q = torch.nn.functional.pad(
+                torch.cumsum(self.b_seq_len - self.b_ready_cache_len, dim=0, dtype=torch.int32), (1, 0)
+            )
+            self.cu_seqlens_k = torch.nn.functional.pad(self.b_start_loc, (1, 0))
+            self.page_table = torch.empty((self.batch_size, self.max_seq_len), dtype=torch.int32).to(input_ids.device)
+            self.page_table.copy_(model.req_manager.req_to_token_indexs[self.b_req_idx, : self.max_seq_len])
         else:
             if self._shared_page_table_buffer is None:
                 FlashAttentionStateInfo.set_page_table_buffer(
@@ -35,6 +39,8 @@ class FlashAttentionStateInfo(LlamaInferStateInfo):
             # Meta information of flashattention for decoding
             self.cu_seqlens_q = torch.arange(0, self.batch_size + 1, dtype=torch.int32, device=input_ids.device)
             self.cu_seqlens_k = torch.nn.functional.pad(self.b_start_loc, (1, 0))
+            b_seq_len_numpy = self.b_seq_len.cpu().numpy()
+            max_seq_len_k = b_seq_len_numpy.max()
             if self.batch_size <= model.graph_max_batch_size and self.max_len_in_batch <= model.graph_max_len_in_batch:
                 self.page_table = self._shared_page_table_buffer[self.microbatch_index][
                     : self.batch_size * self.max_len_in_batch
@@ -44,5 +50,8 @@ class FlashAttentionStateInfo(LlamaInferStateInfo):
                     input_ids.device
                 )
 
-            self.page_table.copy_(model.req_manager.req_to_token_indexs[self.b_req_idx, : self.max_len_in_batch])
+            self.page_table[:, :max_seq_len_k].copy_(
+                model.req_manager.req_to_token_indexs[self.b_req_idx, :max_seq_len_k]
+            )
+            self.page_table[:, max_seq_len_k:].fill_(0)
         return
--- lightllm/models/llama/layer_infer/transformer_layer_infer.py
+++ lightllm/models/llama/layer_infer/transformer_layer_infer.py
@@ -61,15 +61,15 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         return
 
     def _bind_attention(self):
-        self._context_attention_kernel = partial(LlamaTransformerLayerInfer._context_attention_kernel, self)
-
         if get_env_start_args().enable_fa3:
+            self._context_attention_kernel = partial(LlamaTransformerLayerInfer._context_attention_flashattention, self)
             self._token_attention_kernel = partial(
                 LlamaTransformerLayerInfer._token_decode_attention_flashattention, self
             )
             self._copy_kv_to_mem_cache = partial(LlamaTransformerLayerInfer._copy_kv_to_mem_cache_normal, self)
             return
 
+        self._context_attention_kernel = partial(LlamaTransformerLayerInfer._context_attention_kernel, self)
         if "ppl_int8kv" in self.mode:
             self._token_attention_kernel = partial(LlamaTransformerLayerInfer._token_decode_attention_ppl_int8kv, self)
             self._copy_kv_to_mem_cache = partial(LlamaTransformerLayerInfer._copy_kv_to_mem_cache_ppl_int8kv, self)
@@ -252,6 +252,37 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
             )
 
         return o_tensor
+
+    def _context_attention_flashattention(self, q, kv, infer_state: LlamaInferStateInfo, layer_weight, out=None):
+
+        cache_k = infer_state.mem_manager.kv_buffer[self.layer_num_][:, 0 : self.tp_k_head_num_, :].reshape(
+            -1, 1, self.tp_k_head_num_, self.head_dim_
+        )
+        cache_v = infer_state.mem_manager.kv_buffer[self.layer_num_][
+            :, self.tp_k_head_num_ : self.tp_k_head_num_ + self.tp_v_head_num_, :
+        ].reshape(-1, 1, self.tp_v_head_num_, self.head_dim_)
+        q = q.reshape(-1, self.tp_q_head_num_, self.head_dim_)
+        k_descale, v_descale = None, None  # disable quantization
+        Lq = q.shape[-1]
+        sm_scale = 1.0 / (Lq ** 0.5)
+        o = flash_attn_with_kvcache(
+            q=q,
+            k_cache=cache_k,
+            v_cache=cache_v,
+            page_table=infer_state.page_table,
+            cache_seqlens=infer_state.b_seq_len,
+            cu_seqlens_q=infer_state.cu_seqlens_q,
+            cu_seqlens_k_new=infer_state.cu_seqlens_k,
+            max_seqlen_q=infer_state.q_max_seq_len,
+            softmax_scale=sm_scale,
+            causal=True,
+            window_size=(-1, -1),
+            softcap=0.0,
+            k_descale=k_descale,
+            v_descale=v_descale,
+            return_softmax_lse=False,
+        )
+        return o
 
     def _get_o(
         self, input, infer_state: LlamaInferStateInfo, layer_weight: LlamaTransformerLayerWeight
@@ -642,7 +673,6 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
         )
 
     def _token_decode_attention_flashattention(self, q, infer_state: LlamaInferStateInfo, layer_weight, out=None):
-        from lightllm.models.llama.triton_kernel.flash_decoding import token_decode_attention_flash_decoding
 
         cache_k = infer_state.mem_manager.kv_buffer[self.layer_num_][:, 0 : self.tp_k_head_num_, :].reshape(
             -1, 1, self.tp_k_head_num_, self.head_dim_
@@ -662,7 +692,7 @@ class LlamaTransformerLayerInfer(TransformerLayerInferTpl):
             cache_seqlens=infer_state.b_seq_len,
             cu_seqlens_q=infer_state.cu_seqlens_q,
             cu_seqlens_k_new=infer_state.cu_seqlens_k,
-            max_seqlen_q=infer_state.max_len_in_batch,
+            max_seqlen_q=1,
             softmax_scale=sm_scale,
             causal=True,
             window_size=(-1, -1),
