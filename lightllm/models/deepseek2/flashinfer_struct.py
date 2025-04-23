c13eb1b069a930b18570229992e4b8ca936aa75b
baishihao <baishihao@sensetime.com>
2025-04-23 12:22:55 +0800

    fix

--- lightllm/models/deepseek2/flashinfer_struct.py
+++ lightllm/models/deepseek2/flashinfer_struct.py
@@ -29,7 +29,7 @@ class Deepseek2FlashInferStateInfo(Deepseek2InferStateInfo):
                     ]
                 else:
                     self.kv_indices = torch.empty(
-                        model.graph_max_batch_size * self.flashinfer_extra_state.max_seq_length, dtype=torch.int32
+                        self.batch_size * self.flashinfer_extra_state.max_seq_length, dtype=torch.int32
                     ).to(input_ids.device)
                 repack_kv_index(
                     self.req_manager.req_to_token_indexs,
