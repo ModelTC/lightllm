c6fdc415f6d94208c2789fccbdcd608aa076ad85
sangchengmeng <sangchengmeng@sensetime.com>
2025-04-25 12:02:37 +0000

    [fix]mistral and mixtral prompt_cache

--- lightllm/models/mixtral/infer_struct.py
+++ lightllm/models/mixtral/infer_struct.py
@@ -24,8 +24,12 @@ class MixtralInferStateInfo(MistralInferStateInfo):
         self.num_local_experts = model.config["num_local_experts"]
         if self.is_prefill:
             b_seq_len_numpy = self.b_seq_len.cpu().numpy()
+            b_ready_cache_len_numpy = self.b_ready_cache_len.cpu().numpy()
             position_ids = torch.from_numpy(
-                np.concatenate([np.arange(0, b_seq_len_numpy[i]) for i in range(len(b_seq_len_numpy))], axis=0)
+                np.concatenate(
+                    [np.arange(b_ready_cache_len_numpy[i], b_seq_len_numpy[i]) for i in range(len(b_seq_len_numpy))],
+                    axis=0,
+                )
             ).cuda()
             self.position_cos = torch.index_select(model._cos_cached, 0, position_ids).view(position_ids.shape[0], -1)
             self.position_sin = torch.index_select(model._sin_cached, 0, position_ids).view(position_ids.shape[0], -1)
