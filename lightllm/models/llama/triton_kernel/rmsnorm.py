3c7a29811cbe8a2939391978fb3680ccd3d6af0e
wangzaijun <wzjhelloworld@qq.com>
2025-04-29 23:54:16 +0800

    fix qwen3 infer bug.

--- lightllm/models/llama/triton_kernel/rmsnorm.py
+++ lightllm/models/llama/triton_kernel/rmsnorm.py
@@ -57,9 +57,9 @@ def rmsnorm_forward(x: torch.Tensor, weight, eps, out=None):
         raise RuntimeError("This layer norm doesn't support feature dim >= 64KB.")
     # heuristics for number of warps
     num_warps = min(max(BLOCK_SIZE // 256, 1), 8)
-    # print(BLOCK_SIZE, num_warps, "block_size, numwarps")
-    BLOCK_SIZE = 128 * 2 * 2 * 2 * 2 * 2 * 2 * 2
-    num_warps = 8
+    num_warps = triton.next_power_of_2(num_warps)
+    if BLOCK_SIZE > 16384:
+        BLOCK_SIZE = 16384
     # enqueue kernel
     _rms_norm_fwd_fused[(M,)](
         x_arg,
