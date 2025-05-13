46624beea1d537f97033177e1071b7f6b6e5774d
baishihao <baishihao@sensetime.com>
2025-05-13 20:25:20 +0800

    explict init cuda context

--- lightllm/utils/vllm_utils.py
+++ lightllm/utils/vllm_utils.py
@@ -3,11 +3,14 @@ from lightllm.utils.log_utils import init_logger
 
 logger = init_logger(__name__)
 try:
+    if not torch.cuda.is_initialized():
+        torch.cuda.init()
     from vllm import _custom_ops as ops
 
     vllm_ops = ops
     HAS_VLLM = True
     cutlass_scaled_mm = torch.ops._C.cutlass_scaled_mm
+
 except:
     HAS_VLLM = False
     cutlass_scaled_mm = None
