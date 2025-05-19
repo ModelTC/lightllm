0389ef3d698a69edda3c29c409cc1cf4955d58d2
baishihao <baishihao@sensetime.com>
2025-05-19 10:57:40 +0800

    fix

--- lightllm/models/deepseek_mtp/layer_weights/pre_and_post_layer_weight.py
+++ lightllm/models/deepseek_mtp/layer_weights/pre_and_post_layer_weight.py
@@ -6,25 +6,20 @@ class Deepseek3MTPPreAndPostLayerWeight(LlamaPreAndPostLayerWeight):
     def __init__(self, data_type, network_config, mode):
         super().__init__(data_type, network_config, mode)
         # 与DeepseekV3模型共享
-        self.wte_weight_ = None 
+        self.wte_weight_ = None
         self.lm_head_weight_ = None
         self.final_norm_weight_ = None
         return
-    
+
     def load_hf_weights(self, weights):
-        hidden_size = self.network_config_["hidden_size"]
-        split_indexes = np.linspace(0, hidden_size, self.tp_world_size_ + 1, dtype=np.int64)
-        split_start = split_indexes[self.tp_rank_]
-        split_end = split_indexes[self.tp_rank_ + 1]
-        
         if "model.layers.0.eh_proj.weight" in weights:
-            self.eh_proj_weight_ = self._cuda(weights["model.layers.0.eh_proj.weight"][split_start:split_end, :])
+            self.eh_proj_weight_ = self._cuda(weights["model.layers.0.eh_proj.weight"]).t()
         if "model.layers.0.enorm.weight" in weights:
             self.enorm_weight_ = self._cuda(weights["model.layers.0.enorm.weight"])
         if "model.layers.0.hnorm.weight" in weights:
             self.hnorm_weight_ = self._cuda(weights["model.layers.0.hnorm.weight"])
-        return 
-    
+        return
+
     def verify_load(self):
         errors = "weights load not ok"
         weights = [self.eh_proj_weight_, self.enorm_weight_, self.hnorm_weight_]
