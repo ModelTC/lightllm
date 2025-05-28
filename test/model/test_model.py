bba828610efb447c7c55dafcef90571c838ea34d
hiworldwzj <30762946+hiworldwzj@users.noreply.github.com>
2025-05-28 15:55:47 +0800

    fix

--- test/model/test_model.py
+++ test/model/test_model.py
@@ -23,9 +23,9 @@ if __name__ == "__main__":
     import torch
 
     parser = make_argument_parser()
-    parser.add_argument("--batch_size", type=int, default=200, help="batch size")
-    parser.add_argument("--input_len", type=int, default=1000, help="input sequence length")
-    parser.add_argument("--output_len", type=int, default=2000, help="output sequence length")
+    parser.add_argument("--batch_size", type=int, default=2, help="batch size")
+    parser.add_argument("--input_len", type=int, default=4096, help="input sequence length")
+    parser.add_argument("--output_len", type=int, default=128, help="output sequence length")
     parser.add_argument(
         "--profile",
         action="store_true",
