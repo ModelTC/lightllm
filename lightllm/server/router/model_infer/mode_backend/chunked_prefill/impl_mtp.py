e032bf723b6dc7e3cc26b14548007b76eb11a657
shihaobai <1798930569@qq.com>
2025-05-28 16:41:42 +0800

    fix mtp norm and fix chunked

--- lightllm/server/router/model_infer/mode_backend/chunked_prefill/impl_mtp.py
+++ lightllm/server/router/model_infer/mode_backend/chunked_prefill/impl_mtp.py
@@ -9,7 +9,7 @@ from lightllm.server.router.model_infer.mode_backend.generic_pre_process import 
     prepare_prefill_inputs,
 )
 from lightllm.server.router.model_infer.mode_backend.mtp_pre_process import (
-    prepare_mtp_prefill_inputs,
+    prepare_mtp_chunked_prefill_inputs,
     prepare_draft_main_model_decode_inputs,
 )
 from lightllm.server.router.model_infer.mode_backend.generic_post_process import sample
@@ -38,7 +38,7 @@ class ChunkedPrefillWithMTPBackend(ContinuesBatchWithMTPBackend):
 
         if prefill_reqs:
             model_input, run_reqs = prepare_prefill_inputs(
-                prefill_reqs, is_chuncked_mode=False, is_multimodal=self.is_multimodal
+                prefill_reqs, is_chuncked_mode=True, is_multimodal=self.is_multimodal
             )
             model_output = self.model.forward(model_input)
 
@@ -49,26 +49,36 @@ class ChunkedPrefillWithMTPBackend(ContinuesBatchWithMTPBackend):
             next_token_ids, next_token_probs = sample(model_output.logits, run_reqs, self.eos_id)
             next_token_ids = next_token_ids.detach().cpu().numpy()
             next_token_logprobs = torch.log(next_token_probs).detach().cpu().numpy()
-            self._post_handle(
-                run_reqs, next_token_ids, next_token_logprobs, is_chuncked_mode=False, do_filter_finished_reqs=False
-            )
+            prev_step_has_output = [
+                req_obj.get_chuncked_input_token_len() == req_obj.get_cur_total_len() for req_obj in prefill_reqs
+            ]
             # spec prefill: MTP
             last_input_ids_cpu = None
             draft_model_input = model_input
             last_hidden_states = model_output.hidden_states
+            draft_next_token_ids = next_token_ids
             for draft_model_idx in range(self.spec_step):
-                device0_print(f"main {draft_model_input}")
-                draft_model_input, last_input_ids_cpu = prepare_mtp_prefill_inputs(
-                    prefill_reqs, model_input, last_hidden_states, next_token_ids, last_input_ids_cpu
+
+                draft_model_input, last_input_ids_cpu, prev_step_has_output = prepare_mtp_chunked_prefill_inputs(
+                    prefill_reqs,
+                    model_input,
+                    last_hidden_states,
+                    draft_next_token_ids,
+                    draft_model_idx + 1,
+                    prev_step_has_output,
+                    last_input_ids_cpu,
                 )
-                device0_print(f"draft_model_input {draft_model_input}")
+
                 draft_model_output = self.draft_models[draft_model_idx].forward(draft_model_input)
                 draft_next_token_ids, _ = sample(draft_model_output.logits, run_reqs, self.eos_id)
                 draft_next_token_ids = draft_next_token_ids.detach().cpu().numpy()
 
                 last_hidden_states = draft_model_output.hidden_states
-                next_token_ids = draft_next_token_ids
                 self._save_draft_token_ids(draft_next_token_ids, run_reqs, draft_model_idx)
+
+            self._post_handle(
+                run_reqs, next_token_ids, next_token_logprobs, is_chuncked_mode=True, do_filter_finished_reqs=False
+            )
 
         if decode_reqs:
             model_input, run_reqs, mem_indexes_cpu = prepare_draft_main_model_decode_inputs(
@@ -93,9 +103,11 @@ class ChunkedPrefillWithMTPBackend(ContinuesBatchWithMTPBackend):
                 accepted_reqs,
                 next_token_ids[accepted_index],
                 next_token_logprobs[accepted_index],
-                is_chuncked_mode=False,
+                is_chuncked_mode=True,
                 do_filter_finished_reqs=False,
             )
+            self.main_step += 1
+
             # share some inference info with the main model
             draft_model_input = model_input
             draft_model_input.input_ids = next_token_ids_cuda
@@ -118,37 +130,3 @@ class ChunkedPrefillWithMTPBackend(ContinuesBatchWithMTPBackend):
 
         self._overlap_req_init_and_filter(uninit_reqs=uninit_reqs, ok_finished_reqs=ok_finished_reqs, clear_list=True)
         return
-
-    def verify(self, next_token_ids, run_reqs, draft_mem_indexes):
-        accepted_reqs = []
-        accepted_index = []
-        need_free_mem_indexes = []
-        assert next_token_ids.shape[0] % self.spec_stride == 0
-
-        for i, req in enumerate(run_reqs):
-            # main model output
-            if i % self.spec_stride == 0:
-                accepted_reqs.append(req)
-                accepted_index.append(i)
-                continue
-            draft_model_idx = i % self.spec_stride - 1
-            if (
-                self.draft_token_id_map[req.req_idx][draft_model_idx] == next_token_ids[i - 1]
-                and req.cur_accepted_len == draft_model_idx
-            ):
-                accepted_reqs.append(req)
-                accepted_index.append(i)
-                req.cur_accepted_len += 1
-                device0_print(f"req {req.req_idx} accepted, cur_accepted_len {req.cur_accepted_len}")
-            else:
-                need_free_mem_indexes.append(draft_mem_indexes[i])
-        return accepted_reqs, accepted_index, need_free_mem_indexes
-
-    def _save_draft_token_ids(self, draft_next_token_ids, run_reqs, draft_model_idx):
-        batch_size = len(run_reqs) // self.spec_stride
-        for i in range(batch_size):
-            req = run_reqs[self.spec_stride * i]
-            self.draft_token_id_map[req.req_idx][draft_model_idx] = draft_next_token_ids[i + req.cur_accepted_len]
-            #  reset the cur_accepted_len
-            if draft_model_idx == self.spec_step - 1:
-                req.cur_accepted_len = 0
