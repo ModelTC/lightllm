bf11538a61760b707b5c67a48934086605206d3b
wangzaijun <wzjhelloworld@qq.com>
2025-05-30 11:08:30 +0800

    add token_id_out_counter speed test.

--- unit_tests/common/basemodel/triton_kernel/test_gen_sampling_params.py
+++ unit_tests/common/basemodel/triton_kernel/test_gen_sampling_params.py
@@ -15,6 +15,17 @@ def test_token_id_counter():
     true_out = torch.bincount(test_prompt_ids, minlength=test_token_id_counter.shape[0])
     assert torch.equal(test_token_id_counter, true_out)
 
+    test_prompt_ids = torch.randint(0, 50 * 10000, size=(128 * 1024,), device="cuda")
+    test_token_id_counter = torch.zeros((50 * 10000,), device="cuda", dtype=torch.int32)
+
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+    for _ in range(100):
+        token_id_counter(prompt_ids=test_prompt_ids, out_token_id_counter=test_token_id_counter)
+    end_event.record()
+    logger.info(f"test_token_id_count cost time: {start_event.elapsed_time(end_event)} ms")
+
 
 if __name__ == "__main__":
     pytest.main()
