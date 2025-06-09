e60fa06a8cee3a88614bca0e96b8a30c38aec282
baishihao <baishihao@sensetime.com>
2025-06-09 20:04:39 +0800

    refactor continous mtp

--- lightllm/server/router/model_infer/mode_backend/continues_batch/impl_mtp.py
+++ lightllm/server/router/model_infer/mode_backend/continues_batch/impl_mtp.py
@@ -1,9 +1,9 @@
+import os
 import torch
 import numpy as np
 from typing import List, Tuple
+from transformers.configuration_utils import PretrainedConfig
 from lightllm.server.router.model_infer.mode_backend.base_backend import ModeBackend
-from lightllm.utils.infer_utils import calculate_time, mark_start, mark_end
-from lightllm.utils.log_utils import init_logger
 from lightllm.server.router.model_infer.infer_batch import g_infer_context
 from lightllm.server.router.model_infer.mode_backend.generic_pre_process import (
     prepare_prefill_inputs,
@@ -14,13 +14,12 @@ from lightllm.server.router.model_infer.mode_backend.mtp_pre_process import (
     IS_NONE,
 )
 from lightllm.server.router.model_infer.mode_backend.generic_post_process import sample
-import os
-from transformers.configuration_utils import PretrainedConfig
+from lightllm.server.router.model_infer.infer_batch import InferReq
+from lightllm.utils.infer_utils import calculate_time, mark_start, mark_end
+from lightllm.utils.log_utils import init_logger
 from lightllm.models.deepseek_mtp.model import Deepseek3MTPModel
 from lightllm.common.basemodel.infer_lock import g_infer_state_lock
-from lightllm.server.router.model_infer.infer_batch import InferReq
-import copy
-from lightllm.utils.dist_utils import device0_print
+from lightllm.common.basemodel.batch_objs import ModelOutput
 
 
 logger = init_logger(__name__)
@@ -73,13 +72,6 @@ class ContinuesBatchWithMTPBackend(ModeBackend):
 
             self.logger.info(f"loaded mtp model class {self.draft_models[i].__class__}")
 
-        max_req_num = kvargs.get("max_req_num", 1000)
-        self.draft_token_id_map = torch.full(
-            (max_req_num, self.spec_step), fill_value=IS_NONE, dtype=torch.int32, device="cpu"
-        )
-        self.draft_accepted_len = np.zeros((max_req_num, self.spec_step), dtype=np.int32)
-        self.main_step = 0
-
     def prefill(self, reqs: List[Tuple]):
         self._init_reqs(reqs, init_req_obj=False)
         return
@@ -94,7 +86,7 @@ class ContinuesBatchWithMTPBackend(ModeBackend):
 
         if prefill_reqs:
             model_input, run_reqs = prepare_prefill_inputs(
-                prefill_reqs, is_chuncked_mode=False, is_multimodal=self.is_multimodal
+                prefill_reqs, is_chuncked_mode=not self.disable_chunked_prefill, is_multimodal=self.is_multimodal
             )
             model_output = self.model.forward(model_input)
 
@@ -103,33 +95,37 @@ class ContinuesBatchWithMTPBackend(ModeBackend):
             )
 
             next_token_ids, next_token_probs = sample(model_output.logits, run_reqs, self.eos_id)
-            next_token_ids = next_token_ids.detach().cpu().numpy()
-            next_token_logprobs = torch.log(next_token_probs).detach().cpu().numpy()
+            next_token_ids_cpu = next_token_ids.detach().cpu().numpy()
+            next_token_logprobs_cpu = torch.log(next_token_probs).detach().cpu().numpy()
 
             # spec prefill: MTP
-            last_input_ids_cpu = None
             draft_model_input = model_input
             last_hidden_states = model_output.hidden_states
-            draft_next_token_ids = next_token_ids
+            # For simplicity, we also save the output of the main model to req.mtp_gen_token_ids.
+            self._save_prefill_draft_tokens(next_token_ids, run_reqs, -1)
             for draft_model_idx in range(self.spec_step):
-                draft_model_input, last_input_ids_cpu = prepare_mtp_prefill_inputs(
-                    prefill_reqs, model_input, last_hidden_states, draft_next_token_ids, last_input_ids_cpu
+                draft_model_input = prepare_mtp_prefill_inputs(
+                    prefill_reqs,
+                    model_input,
+                    last_hidden_states,
+                    draft_model_idx,
+                    is_chunked_mode=not self.disable_chunked_prefill,
                 )
                 draft_model_output = self.draft_models[draft_model_idx].forward(draft_model_input)
-                draft_next_token_ids, _ = sample(draft_model_output.logits, run_reqs, self.eos_id)
-                draft_next_token_ids = draft_next_token_ids.detach().cpu().numpy()
-
+                _, draft_next_token_ids_cpu = self._gen_draft_tokens(draft_model_output)
                 last_hidden_states = draft_model_output.hidden_states
-                self._save_draft_token_ids(draft_next_token_ids, run_reqs, draft_model_idx)
+                self._save_prefill_draft_tokens(draft_next_token_ids_cpu, run_reqs, draft_model_idx)
 
             self._post_handle(
-                run_reqs, next_token_ids, next_token_logprobs, is_chuncked_mode=False, do_filter_finished_reqs=False
+                run_reqs,
+                next_token_ids_cpu,
+                next_token_logprobs_cpu,
+                is_chuncked_mode=not self.disable_chunked_prefill,
+                do_filter_finished_reqs=False,
             )
 
         if decode_reqs:
-            model_input, run_reqs, mem_indexes_cpu = prepare_draft_main_model_decode_inputs(
-                decode_reqs, self.draft_token_id_map
-            )
+            model_input, run_reqs, mem_indexes_cpu = prepare_draft_main_model_decode_inputs(decode_reqs)
             model_output = self.model.forward(model_input)
             assert model_output.logits.shape[0] % self.spec_stride == 0
 
@@ -137,37 +133,36 @@ class ContinuesBatchWithMTPBackend(ModeBackend):
                 uninit_reqs=uninit_reqs, ok_finished_reqs=ok_finished_reqs, clear_list=True
             )
 
-            next_token_ids_cuda, next_token_probs = sample(model_output.logits, run_reqs, self.eos_id)
-            next_token_ids = next_token_ids_cuda.detach().cpu().numpy()
-            next_token_logprobs = torch.log(next_token_probs).detach().cpu().numpy()
+            next_token_ids, next_token_probs = sample(model_output.logits, run_reqs, self.eos_id)
+            next_token_ids_cpu = next_token_ids.detach().cpu().numpy()
+            next_token_logprobs_cpu = torch.log(next_token_probs).detach().cpu().numpy()
 
             # verify
-            accepted_reqs, accepted_index, need_free_mem_indexes = self.verify(
+            accepted_reqs, accepted_index, need_free_mem_indexes = self._verify(
                 next_token_ids, run_reqs, mem_indexes_cpu
             )
+            self._save_decode_draft_token_ids(next_token_ids, run_reqs, -1)
             self._post_handle(
                 accepted_reqs,
-                next_token_ids[accepted_index],
-                next_token_logprobs[accepted_index],
-                is_chuncked_mode=False,
+                next_token_ids_cpu[accepted_index],
+                next_token_logprobs_cpu[accepted_index],
+                is_chuncked_mode=not self.disable_chunked_prefill,
                 do_filter_finished_reqs=False,
             )
-            self.main_step += 1
 
             # share some inference info with the main model
             draft_model_input = model_input
-            draft_model_input.input_ids = next_token_ids_cuda
+            draft_model_input.input_ids = next_token_ids
             draft_model_input.hidden_states = model_output.hidden_states
             # process the draft model output
             for draft_model_idx in range(self.spec_step):
                 # spec decode: MTP
                 draft_model_output = self.draft_models[draft_model_idx].forward(draft_model_input)
-                draft_next_token_ids, _ = sample(draft_model_output.logits, run_reqs, self.eos_id)
+                draft_next_token_ids, draft_next_token_ids_cpu = self._gen_draft_tokens(draft_model_output)
                 # prepare inputs for the next draft model
                 draft_model_input.input_ids = draft_next_token_ids
                 draft_model_input.hidden_states = draft_model_output.hidden_states
-                draft_next_token_ids_numpy = draft_next_token_ids.detach().cpu().numpy()
-                self._save_draft_token_ids(draft_next_token_ids_numpy, run_reqs, draft_model_idx)
+                self._save_decode_draft_token_ids(draft_next_token_ids_cpu, run_reqs, draft_model_idx)
 
             if need_free_mem_indexes:
                 g_infer_state_lock.acquire()
@@ -177,39 +172,57 @@ class ContinuesBatchWithMTPBackend(ModeBackend):
         self._overlap_req_init_and_filter(uninit_reqs=uninit_reqs, ok_finished_reqs=ok_finished_reqs, clear_list=True)
         return
 
-    def verify(self, next_token_ids, run_reqs, draft_mem_indexes):
+    def _gen_draft_tokens(self, model_output: ModelOutput):
+        logits = model_output.logits
+        probs = torch.softmax(logits, dim=-1)
+        draft_next_token_ids = torch.argmax(probs, dim=-1)
+        return draft_next_token_ids, draft_next_token_ids.detach().cpu().numpy()
+
+    def _verify(self, next_token_ids: torch.Tensor, run_reqs: List[InferReq], draft_mem_indexes: torch.Tensor):
         accepted_reqs = []
         accepted_index = []
         need_free_mem_indexes = []
         assert next_token_ids.shape[0] % self.spec_stride == 0
-
-        for i, req in enumerate(run_reqs):
-            # main model output
-            if i % self.spec_stride == 0:
-                accepted_reqs.append(req)
-                accepted_index.append(i)
-                continue
-            draft_model_idx = i % self.spec_stride - 1
-            if (
-                self.draft_token_id_map[req.req_idx][draft_model_idx] == next_token_ids[i - 1]
-                and req.cur_accepted_len == draft_model_idx
-            ):
-                accepted_reqs.append(req)
-                accepted_index.append(i)
-                req.cur_accepted_len += 1
-            else:
-                need_free_mem_indexes.append(draft_mem_indexes[i])
+        batch_size = next_token_ids.shape[0] // self.spec_stride
+        for b in range(batch_size):
+            req = run_reqs[b % self.spec_stride]
+            req_start_idx = b * self.spec_stride
+            req_end_idx = (b + 1) * self.spec_stride
+            for step_idx in range(self.spec_stride):
+                if step_idx == 0 or req.mtp_gen_token_ids[step_idx] == next_token_ids[req_start_idx + step_idx - 1]:
+                    accepted_reqs.append(req)
+                    accepted_index.append(req_start_idx + step_idx)
+                    req.mtp_cur_accepted_len += 1
+                else:
+                    need_free_mem_indexes.append(draft_mem_indexes[req_start_idx + step_idx : req_end_idx])
+                    break
         return accepted_reqs, accepted_index, need_free_mem_indexes
 
-    def _save_draft_token_ids(self, draft_next_token_ids, run_reqs, draft_model_idx):
+    def _save_prefill_draft_tokens(
+        self, draft_next_token_ids: torch.Tensor, run_reqs: List[InferReq], draft_model_idx: int
+    ):
+        batch_size = len(run_reqs)
+        for i in range(batch_size):
+            req = run_reqs[i]
+            # if the request has unfinished chunked tokens, skip it.
+            if req.get_chunked_input_token_len() < req.get_cur_total_len():
+                continue
+            req.mtp_gen_token_ids.append(draft_next_token_ids[i])
+            #  reset the mtp status
+            if draft_model_idx == self.spec_step - 1:
+                req.mtp_gen_token_ids = []
+
+    def _save_decode_draft_token_ids(
+        self, draft_next_token_ids: torch.Tensor, run_reqs: List[InferReq], draft_model_idx: int
+    ):
         batch_size = len(run_reqs) // self.spec_stride
         for i in range(batch_size):
             req = run_reqs[self.spec_stride * i]
-            self.draft_token_id_map[req.req_idx][draft_model_idx] = draft_next_token_ids[
-                i * self.spec_stride + req.cur_accepted_len
-            ]
-            #  reset the cur_accepted_len
+            # append the draft token
+            req.mtp_gen_token_ids.append(draft_next_token_ids[i * self.spec_stride + req.mtp_cur_accepted_len])
+            #  reset the mtp status
             if draft_model_idx == self.spec_step - 1:
                 if self.is_master_in_dp:
                     req.set_total_accepted_len()
-                req.cur_accepted_len = 0
+                req.mtp_cur_accepted_len = 0
+                req.mtp_gen_token_ids = []
--- lightllm/server/router/model_infer/mode_backend/mtp_pre_process.py
+++ lightllm/server/router/model_infer/mode_backend/mtp_pre_process.py
@@ -9,63 +9,30 @@ IS_NONE = -1
 
 
 def prepare_mtp_prefill_inputs(
-    req_objs: List[InferReq], model_input: ModelInput, last_hidden_states, tgt_input_ids, last_input_ids_cpu=None
-):
-    input_ids = []
-    for i, req in enumerate(req_objs):
-        if last_input_ids_cpu is None:
-            input_token_ids = req.get_input_token_ids()
-        else:
-            input_token_ids = last_input_ids_cpu[i]
-        input_token_ids = np.roll(input_token_ids, -1)
-        input_token_ids[-1] = tgt_input_ids[i]
-        input_ids.append(input_token_ids[req.cur_kv_len :])
-    input_ids_cpu = input_ids
-    input_ids = np.concatenate(input_ids, dtype=np.int64)
-    input_ids = torch.tensor(input_ids, dtype=torch.int64, device="cuda")
-    model_input.input_ids = input_ids
-    # mtp embedding
-    model_input.hidden_states = last_hidden_states
-    return model_input, input_ids_cpu
-
-
-def prepare_mtp_chunked_prefill_inputs(
     req_objs: List[InferReq],
     model_input: ModelInput,
-    last_hidden_states,
-    tgt_input_ids,
-    shift,
-    prev_step_has_output,
-    last_input_ids_cpu=None,
-    pad_for_empty_batch=False,
+    last_hidden_states: torch.Tensor,
+    draft_model_idx: int,
+    is_chunked_mode: bool,
 ):
-    if len(req_objs) == 0:
-        assert pad_for_empty_batch
-        return model_input, np.array([1]), prev_step_has_output
-    
     input_ids = []
     for i, req in enumerate(req_objs):
-        if last_input_ids_cpu is None or not prev_step_has_output[i]:
-            input_token_ids, is_last_chunked = req.get_chunked_input_token_ids_shift(shift)
-            if prev_step_has_output[i]:
-                input_token_ids[-1] = tgt_input_ids[i]
-            prev_step_has_output[i] = is_last_chunked
+        if is_chunked_mode:
+            input_token_ids = req.get_chunked_input_token_ids_shift(draft_model_idx)
         else:
-            input_token_ids = last_input_ids_cpu[i]
-            input_token_ids = np.roll(input_token_ids, -1)
-            input_token_ids[-1] = tgt_input_ids[i]
-            prev_step_has_output[i] = True
+            input_token_ids = req.get_input_token_ids_shift(draft_model_idx)
         input_ids.append(input_token_ids[req.cur_kv_len :])
-    input_ids_cpu = input_ids
     input_ids = np.concatenate(input_ids, dtype=np.int64)
     input_ids = torch.tensor(input_ids, dtype=torch.int64, device="cuda")
     model_input.input_ids = input_ids
     # mtp embedding
     model_input.hidden_states = last_hidden_states
-    return model_input, input_ids_cpu, prev_step_has_output
+    return model_input
 
 
-def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token_id_map, pad_for_empty_batch: bool = False, pad_to_tgt_batch_size : int = None):
+def prepare_draft_main_model_decode_inputs(
+    req_objs: List[InferReq], pad_for_empty_batch: bool = False, pad_to_tgt_batch_size: int = None
+):
     run_reqs = []
     nopad_total_token_num = 0
     nopad_max_len_in_batch = 0
@@ -73,24 +40,16 @@ def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token
     nopad_b_req_idx = []
     nopad_b_seq_len = []
     for req in req_objs:
-        run_reqs.append(req)
-        nopad_b_req_idx.append(req.req_idx)
-        input_id = req.get_last_gen_token()
-        seq_len = req.get_cur_total_len()
-        assert req.cur_kv_len == seq_len - 1
-        nopad_b_seq_len.append(seq_len)
-        input_ids.append(input_id)
-        nopad_total_token_num += seq_len
-        nopad_max_len_in_batch = max(nopad_max_len_in_batch, seq_len)
-
-        # mtp step
-        for step in range(draft_token_id_map.shape[1]):
+        # req.mtp_gen_token_ids[0]: output of main model
+        # req.mtp_gen_token_ids[1:]: output of draft models
+        for step in range(len(req.mtp_gen_token_ids)):
             run_reqs.append(req)
             nopad_b_req_idx.append(req.req_idx)
-            seq_len = req.get_cur_total_len() + step + 1
-            assert req.cur_kv_len == seq_len - step - 2
+            seq_len = req.get_cur_total_len() + step
+            assert req.cur_kv_len == seq_len - step - 1
             nopad_b_seq_len.append(seq_len)
-            input_ids.append(draft_token_id_map[req.req_idx][step])
+            input_ids.append(req.mtp_gen_token_ids[step])
+            nopad_total_token_num += seq_len
             nopad_max_len_in_batch = max(nopad_max_len_in_batch, seq_len)
 
     padded_req_num = 0
@@ -100,7 +59,7 @@ def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token
     elif len(req_objs) == 0:
         assert pad_for_empty_batch
         padded_req_num = 1
-        
+
     for _ in range(padded_req_num):
         input_ids.append(1)
         seq_len = 2
@@ -108,19 +67,19 @@ def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token
         nopad_b_seq_len.append(seq_len)
         nopad_total_token_num += seq_len
         nopad_max_len_in_batch = max(nopad_max_len_in_batch, seq_len)
-            
+
     input_ids = torch.tensor(input_ids, dtype=torch.int64, device="cuda")
     nopad_b_req_idx = torch.tensor(nopad_b_req_idx, dtype=torch.int32, device="cuda")
     nopad_b_seq_len = torch.tensor(nopad_b_seq_len, dtype=torch.int32, device="cuda")
 
     # dynamic prompt cache 准备 token
     g_infer_state_lock.acquire()
-    num_tokens_to_alloc = input_ids.shape[0] - padded_req_num 
+    num_tokens_to_alloc = input_ids.shape[0] - padded_req_num
     if g_infer_context.radix_cache is not None:
         g_infer_context.radix_cache.free_radix_cache_to_get_enough_token(num_tokens_to_alloc)
     mem_indexes_cpu = g_infer_context.req_manager.mem_manager.alloc(num_tokens_to_alloc)
     g_infer_state_lock.release()
-    
+
     # 如果有填充请求，需要将填充请求的 mem_indexes 设置为 HOLD_TOKEN_MEMINDEX
     if padded_req_num > 0:
         padding_mem_indexs = torch.full(
@@ -130,9 +89,9 @@ def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token
             device="cpu",
         )
         mem_indexes_cpu = torch.cat((mem_indexes_cpu, padding_mem_indexs), dim=0)
-        
+
     mem_indexes = mem_indexes_cpu.cuda()
-    
+
     model_input = ModelInput(
         batch_size=len(run_reqs),
         total_token_num=nopad_total_token_num,
