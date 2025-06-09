0a64c2aa017eda9806711822e7f741bd38fd8382
sufubao <47234901+sufubao@users.noreply.github.com>
2025-06-09 10:26:41 +0800

    Deepseek MTP for dp backend (#923)

--- lightllm/server/router/model_infer/mode_backend/mtp_pre_process.py
+++ lightllm/server/router/model_infer/mode_backend/mtp_pre_process.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import List, Tuple
 from lightllm.server.router.model_infer.infer_batch import InferReq, g_infer_context
 from lightllm.common.basemodel.infer_lock import g_infer_state_lock
-from lightllm.common.basemodel.batch_objs import ModelInput, ModelOutput
+from lightllm.common.basemodel.batch_objs import ModelInput
 
 IS_NONE = -1
 
@@ -37,7 +37,12 @@ def prepare_mtp_chunked_prefill_inputs(
     shift,
     prev_step_has_output,
     last_input_ids_cpu=None,
+    pad_for_empty_batch=False,
 ):
+    if len(req_objs) == 0:
+        assert pad_for_empty_batch
+        return model_input, np.array([1]), prev_step_has_output
+    
     input_ids = []
     for i, req in enumerate(req_objs):
         if last_input_ids_cpu is None or not prev_step_has_output[i]:
@@ -60,7 +65,7 @@ def prepare_mtp_chunked_prefill_inputs(
     return model_input, input_ids_cpu, prev_step_has_output
 
 
-def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token_id_map):
+def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token_id_map, pad_for_empty_batch: bool = False, pad_to_tgt_batch_size : int = None):
     run_reqs = []
     nopad_total_token_num = 0
     nopad_max_len_in_batch = 0
@@ -88,17 +93,46 @@ def prepare_draft_main_model_decode_inputs(req_objs: List[InferReq], draft_token
             input_ids.append(draft_token_id_map[req.req_idx][step])
             nopad_max_len_in_batch = max(nopad_max_len_in_batch, seq_len)
 
+    padded_req_num = 0
+    if pad_to_tgt_batch_size is not None:
+        assert pad_to_tgt_batch_size > 0
+        padded_req_num = max(0, pad_to_tgt_batch_size - len(run_reqs))
+    elif len(req_objs) == 0:
+        assert pad_for_empty_batch
+        padded_req_num = 1
+        
+    for _ in range(padded_req_num):
+        input_ids.append(1)
+        seq_len = 2
+        nopad_b_req_idx.append(g_infer_context.req_manager.HOLD_REQUEST_ID)
+        nopad_b_seq_len.append(seq_len)
+        nopad_total_token_num += seq_len
+        nopad_max_len_in_batch = max(nopad_max_len_in_batch, seq_len)
+            
     input_ids = torch.tensor(input_ids, dtype=torch.int64, device="cuda")
     nopad_b_req_idx = torch.tensor(nopad_b_req_idx, dtype=torch.int32, device="cuda")
     nopad_b_seq_len = torch.tensor(nopad_b_seq_len, dtype=torch.int32, device="cuda")
 
     # dynamic prompt cache 准备 token
     g_infer_state_lock.acquire()
+    num_tokens_to_alloc = input_ids.shape[0] - padded_req_num 
     if g_infer_context.radix_cache is not None:
-        g_infer_context.radix_cache.free_radix_cache_to_get_enough_token(input_ids.shape[0])
-    mem_indexes_cpu = g_infer_context.req_manager.mem_manager.alloc(input_ids.shape[0])
-    mem_indexes = mem_indexes_cpu.cuda()
+        g_infer_context.radix_cache.free_radix_cache_to_get_enough_token(num_tokens_to_alloc)
+    mem_indexes_cpu = g_infer_context.req_manager.mem_manager.alloc(num_tokens_to_alloc)
     g_infer_state_lock.release()
+    
+    # 如果有填充请求，需要将填充请求的 mem_indexes 设置为 HOLD_TOKEN_MEMINDEX
+    if padded_req_num > 0:
+        padding_mem_indexs = torch.full(
+            (padded_req_num,),
+            fill_value=g_infer_context.req_manager.mem_manager.HOLD_TOKEN_MEMINDEX,
+            dtype=torch.int32,
+            device="cpu",
+        )
+        mem_indexes_cpu = torch.cat((mem_indexes_cpu, padding_mem_indexs), dim=0)
+        
+    mem_indexes = mem_indexes_cpu.cuda()
+    
     model_input = ModelInput(
         batch_size=len(run_reqs),
         total_token_num=nopad_total_token_num,
