cd153e9d7fdcae78bb53fe72b8598d08fa7bb725
wangzaijun <wzjhelloworld@qq.com>
2025-06-12 13:56:32 +0800

    fix

--- lightllm/server/api_start.py
+++ lightllm/server/api_start.py
@@ -121,10 +121,6 @@ def normal_or_p_d_start(args):
     else:
         assert args.mtp_step > 0
 
-    if args.spec_algo == "MTP":
-        assert args.spec_step > 0, "spec_step must be greater than 0"
-        assert args.spec_model_dir is not None, "spec_model_dir must be set"
-
     # 检查GPU数量是否足够
     if args.visual_gpu_ids is None:
         args.visual_gpu_ids = list(range(args.visual_dp * args.visual_tp))
