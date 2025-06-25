d1c7396b606498e014f30434fde4faf220e0e461
pigKiller <22373017@buaa.edu.cn>
2025-06-25 11:20:28 +0800

    feat: remove unnecessary files

--- server_master.sh
+++
@@ -1,6 +0,0 @@
-python3 -m lightllm.server.api_server \
-    --model_dir /mnt/youwei-data/zhuohang/model/Qwen/Qwen2.5-14B \
-    --run_mode "pd_master" \
-    --host 127.0.1.1 \
-    --port 60011 \
-    --pd_chunk_size 4096
--- test.sh
+++
@@ -1,8 +0,0 @@
-python3 test/benchmark_client.py \
-  --url http://127.0.1.1:60011/generate \
-  --num_clients 1 \
-  --tokenizer_path /mnt/youwei-data/zhuohang/model/Qwen/Qwen2.5-14B \
-  --output_len 16384 \
-  --server_api lightllm \
-  --dump_file result.json \
-  --seed 42
